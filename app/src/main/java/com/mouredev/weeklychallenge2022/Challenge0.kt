121d9545d1ca745d27a0db1529a706e5dff61a26
Lucía Gálvez Durán <lucia.galudev@gmail.com>
2021-12-28 13:00:45 +0100

    Solución Galudev

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,5 +21,17 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-
+    for (i in 1..100) {
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
+                println("fizzbuzz")
+            } else {
+                println("fizz")
+            }
+        } else if (i % 5 == 0) {
+            println("buzz")
+        } else {
+            println(i)
+        }
+    }
 }
