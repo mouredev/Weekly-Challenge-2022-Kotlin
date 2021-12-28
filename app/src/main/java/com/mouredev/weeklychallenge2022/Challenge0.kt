489ef8defd0630be039d3966dc9e7b9d58bbc462
Sergitxin22 <sergiohablador@gmail.com>
2021-12-28 19:59:52 +0100

    Solución Reto #0

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,5 +21,7 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-
+    for(i in 1..100){
+        if(i%3 == 0 && i%5 == 0) println("fizzbuzz") else if(i%3 == 0) println("fizz") else if(i%5 == 0) println("buzz") else println(i)
+    }
 }
