81afa69d94c98379d0c345491ecad0657fdaf19d
Dani Losada <losada.dani@gmail.com>
2021-12-28 15:46:17 +0100

    Corrección codigo

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -27,8 +27,8 @@ fun main() {
 }
 
 fun printFizzBuzz(number: Int): String {
+    if(number%3 == 0 && number%5 == 0) return "fizzbuzz"
     if(number%3 == 0) return "fizz"
     if(number%5 == 0) return "buzz"
-    if(number%3 == 0 && number%5 == 0) return "fizzbuzz"
     return number.toString()
 }
