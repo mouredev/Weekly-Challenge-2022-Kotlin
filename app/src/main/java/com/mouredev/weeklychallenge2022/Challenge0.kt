5297b27e3f39f6b64c90063e7722b9a986dbf769
davidthar <47674026+davidthar@users.noreply.github.com>
2021-12-28 17:42:23 +0100

    Reto #0 Completado

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,5 +21,11 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
+    for(num in 1..100){
+        if(num%3==0 && num%5==0) println("fizzbuzz")
+        else if(num%3==0) println("fizz")
+        else if(num%5==0) println("buzz")
+        else println(num)
+    }
 
 }
