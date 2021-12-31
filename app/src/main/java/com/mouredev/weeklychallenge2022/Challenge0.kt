37448b3fe85186335a5adf662a9359dc47883cde
Rubén Donate Serrano <ruben.donate.serrano@gmail.com>
2021-12-31 13:42:59 +0100

    My solution for the challenge0

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,5 +21,14 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
+    for (i in 1..100){
+        var data = ""
+        when {
+            (i % 3 == 0 && i % 5 == 0) -> println("fizzbuzz")
+            i % 3 == 0 -> println("fizz")
+            i % 5 == 0 -> println("buzz")
+            else -> println("$i")
+        }
+    }
 
 }
