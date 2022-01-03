985138c5152700c06fb6c6ce320bd86d1ca57e40
Brais Moure <braismoure@mouredev.com>
2022-01-03 21:31:01 +0100

    Resolución Reto #0 y enunciado Reto #1

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -6,7 +6,6 @@ package com.mouredev.weeklychallenge2022
  * Fecha publicación enunciado: 27/12/21
  * Fecha publicación resolución: 03/01/22
  * Dificultad: FÁCIL
- *
  * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
  * - Múltiplos de 3 por la palabra "fizz".
  * - Múltiplos de 5 por la palabra "buzz".
