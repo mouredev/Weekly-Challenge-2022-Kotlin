d66102b6bcb27347818de0ebcff32969bd6558f3
Brais Moure <braismoure@mouredev.com>
2022-01-03 21:31:01 +0100

    Resolución Reto #0 y enunciado Reto #1

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -12,7 +12,7 @@ package com.mouredev.weeklychallenge2022
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  *
  * Información adicional:
- * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🗓reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
  * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
  * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
@@ -20,16 +20,7 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-    println("Moure Solution:")
-    mouredev()
-    println()
-    println("My Solution:")
-    mySolution()
-    println()
-    println("When Solution:")
-    whenSolution()
-}
-fun mouredev() {
+
     for (index in 1..100) {
         val divisibleByThree = index % 3 == 0
         val divisibleByFive = index % 5 == 0
@@ -41,30 +32,6 @@ fun mouredev() {
             println("buzz")
         } else {
             println(index)
-        }
-    }
-}
-
-fun mySolution() {
-    for (f in 1..100){
-        if (f%3 == 0 && f%5 == 0) {
-            println("fizzbuzz")
-        } else if (f%3 == 0){
-            println("fizz")
-        } else if (f%5 == 0){
-            println("buzz")
-        } else {
-            println(f)
-        }
-    }
-}
-fun whenSolution() {
-    for (i in 1..100){
-        when {
-            (i % 3 == 0 && i % 5 == 0) -> print("fizzbuzz\n")
-            i % 3 == 0 -> print("fizz\n")
-            i % 5 == 0 -> print("buzz\n")
-            else -> println(i)
         }
     }
 }
