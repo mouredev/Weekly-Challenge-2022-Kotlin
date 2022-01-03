23ee6338a62467b8700352b93586383e278361d3
migue <miguelexd@hotmail.com>
2022-01-03 10:56:08 +0100

    Solucion reto 0

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,5 +21,12 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-
+    for (i in 1..100){
+        when {
+            (i % 3 == 0 && i % 5 == 0) -> print("fizzbuzz\n")
+            i % 3 == 0 -> print("fizz\n")
+            i % 5 == 0 -> print("buzz\n")
+            else ->println(i)
+        }
+    }
 }
