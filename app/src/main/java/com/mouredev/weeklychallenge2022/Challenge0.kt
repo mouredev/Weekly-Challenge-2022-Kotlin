35ffa4eea3b0f372e14682d5a695b8e8fa337bf4
Adrian Castro Alonso <adrian.castro.2003@icloud.com>
2022-01-03 12:00:32 +0100

    First Challenge_3/01/2021

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,5 +21,15 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-
+    for (i in 1..100) {
+        if (i % 3 == 0) {
+            println("fizz")
+        } else if (i % 5 == 0) {
+            println("buzz")
+        } else if (i % 3 == 0 && i % 5 == 0) {
+            println("fizzbuzz")
+        } else {
+            println(i)
+        }
+    }
 }
