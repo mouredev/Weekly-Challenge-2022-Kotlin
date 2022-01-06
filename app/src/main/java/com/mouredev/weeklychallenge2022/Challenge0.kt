b43c9bd8a2957b91afbc6da810e0347e46c2361b
Rubén Donate Serrano <ruben.donate.serrano@gmail.com>
2022-01-06 13:36:37 +0100

    Change Solution Challenge0 for solution of MoureDev after Merge with solution Challenge1.

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -20,15 +20,6 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-    for (i in 1..100){
-        when {
-            (i % 3 == 0 && i % 5 == 0) -> print("fizzbuzz\n")
-            i % 3 == 0 -> print("fizz\n")
-            i % 5 == 0 -> print("buzz\n")
-            else -> print("$i\n")
-        }
-    }
-
     for (index in 1..100) {
         val divisibleByThree = index % 3 == 0
         val divisibleByFive = index % 5 == 0
