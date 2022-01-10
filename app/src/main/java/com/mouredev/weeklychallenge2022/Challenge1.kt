a794b02825b3ba0ccf5390c65473739d7eeb8a8e
Sergitxin22 <sergiohablador@gmail.com>
2022-01-10 17:35:35 +0100

    Solución Reto #1

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
@@ -1,5 +1,8 @@
 package com.mouredev.weeklychallenge2022
 
+import java.util.*
+import kotlin.jvm.JvmStatic
+
 /*
  * Reto #1
  * ¿ES UN ANAGRAMA?
@@ -21,5 +24,64 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
+    val anagrama = esAnagrama("Holaa", "hLoaa")
+    //val anagrama = esAnagrama2("Holaa", "hLoaa")
+    if (anagrama) {
+        println("Las dos palabras son anagramas")
+    } else {
+        println("Las dos palabras NO son anagramas")
+    }
+}
 
+// Forma 1
+private fun esAnagrama(palabra1: String, palabra2: String): Boolean {
+    // Convertir String palabra1 en array
+    val StrPalabra1 = palabra1.lowercase().toCharArray()
+
+    // Convertir String palabra2 en array
+    val StrPalabra2 = palabra2.lowercase().toCharArray()
+
+    var resp = false
+    if (palabra1.equals(palabra2, ignoreCase = true)) {
+        resp = false
+    } else {
+        if (StrPalabra1.size == StrPalabra2.size) { // Las dos palabras tienen el mismo numero de letras
+            var i = 0
+            var letrasExistentes = 0
+            do { // Mientras la posicion exista 0
+                var j = 0
+                while (j < StrPalabra2.size && StrPalabra1[i] != StrPalabra2[j]) {
+                    j++
+                }
+                if (j < StrPalabra2.size) {
+                    println("Existe la letra " + StrPalabra1[i] + " en la posicion " + j)
+                    letrasExistentes++
+                    StrPalabra2[j] = '0'
+                } else {
+                    println("No existe la letra " + StrPalabra1[i])
+                    resp = false
+                }
+                i++
+            } while (i < StrPalabra1.size)
+            if (letrasExistentes == StrPalabra2.size) {
+                resp = true
+            }
+        } else {
+            resp = false
+        }
+    }
+    return resp
+}
+
+// Forma 2
+private fun esAnagrama2(palabra1: String, palabra2: String): Boolean {
+    val palabra1Char = palabra1.lowercase(Locale.getDefault())
+    val palabra2Char = palabra2.lowercase(Locale.getDefault())
+
+    val palabra1 = palabra1Char.replace("[^\\\\dA-Za-z]".toRegex(), "").toCharArray()
+    val palabra2 = palabra2Char.replace("[^\\\\dA-Za-z]".toRegex(), "").toCharArray()
+
+    Arrays.sort(palabra1)
+    Arrays.sort(palabra2)
+    return Arrays.equals(palabra1, palabra2)
 }
