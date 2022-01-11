336216cef1636eead8399550768c8ccef9543ca4
davidthar <47674026+davidthar@users.noreply.github.com>
2022-01-11 17:35:17 +0100

    Reto mouredev#2 Completado

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge2.kt
@@ -0,0 +1,34 @@
+package com.mouredev.weeklychallenge2022
+
+/*
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+fun main() {
+    val arrayFibonacci = mutableListOf<Long>(0,1)
+
+    while (arrayFibonacci.size < 50){
+        val previousNumber = arrayFibonacci[arrayFibonacci.size-1]
+        val previousPreviousNumber = arrayFibonacci[arrayFibonacci.size-2]
+        val newNumber = previousNumber + previousPreviousNumber
+
+        arrayFibonacci.add(newNumber)
+    }
+    
+    println(arrayFibonacci)
+}
