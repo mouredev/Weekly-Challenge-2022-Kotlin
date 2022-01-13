17b2abca90c0e5065a140d011d93b755122894dd
Brais <81094589+braiso-22@users.noreply.github.com>
2022-01-14 00:12:30 +0100

    Refactorizado Challenge2(Fibonacci)

--- Weekly-Challenge-2022-Java/src/weeklychallenge2022/Challenge2.java
+++ Weekly-Challenge-2022-Java/src/weeklychallenge2022/Challenge2.java
@@ -33,30 +33,27 @@ public class Challenge2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        long inicial1 = 0,
-                inicial2 = 1;
 
-        fibonacci(inicial1, inicial2);
-
+        fibonacci(0, 1);
     }
 
-    public static void fibonacci(long primero, long segundo) {
+    public static void fibonacci(long vAnterior1, long vAnterior2) {
 
-        System.out.print(primero + " " + segundo + " ");
-
-        int largo = 50;
         long resultado;
-        
-        //empezamos en el 3 porque el 1 y el 2 ya se han mostrado
+        int largo = 50;
+
+        System.out.print(vAnterior1 + ", " + vAnterior2);
+
+        // Empezamos en el 3 porque el 1 y el 2 ya se han mostrado
         for (int i = 3; i <= largo; i++) {
-            resultado = primero + segundo;
-            
-            System.out.print(resultado + " ");
-            if(i%10==0)System.out.println("");
-            primero = segundo;
-            segundo = resultado;
+            resultado = vAnterior1 + vAnterior2;
+
+            System.out.print(", " + resultado);
+
+            vAnterior1 = vAnterior2;
+            vAnterior2 = resultado;
         }
-
+        
+        System.out.print("\n");
     }
-
 }
