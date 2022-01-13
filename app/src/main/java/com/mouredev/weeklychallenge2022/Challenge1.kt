a31640a906575f72017a88f84a37d9e207db5234
Juan Manuel Acosta <jmab2k@gmail.com>
2022-01-13 18:56:12 +0100

    Revert "Merge remote-tracking branch 'origin/main' into Solucionreto#1"
    This reverts commit d5fd61882a2913d412a110408857ad8fc2db557e.

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
@@ -13,7 +13,7 @@ package com.mouredev.weeklychallenge2022
  * Dos palabras exactamente iguales no son anagrama.
  *
  * Información adicional:
- * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🗓reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
  * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
  * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
@@ -21,12 +21,42 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
-    println(isAnagram("amor", "roma"))
+
+    var resultAnagrama = anagrama("ana", "aan")
+    println("el resultado de comparar es $resultAnagrama")
+
+
+
+
+
+
+}
+fun anagrama(
+    word1: String,
+    word2: String
+): Boolean {
+    val arrayWord1 : CharArray = word1.toCharArray()
+    val arrayWord2 : CharArray = word2.toCharArray()
+
+    var resultadoWord1 = true
+    var resultadoWord2 = true
+    if ((arrayWord1.size != arrayWord2.size) || (word1==word2)){
+        return false
+
+    }
+    else {
+        for (i in arrayWord1.indices){
+            if (!arrayWord2.contains (arrayWord1[i])){
+                resultadoWord1= false
+            }
+            if (!arrayWord1.contains (arrayWord2[i])){
+                resultadoWord2= false
+            }
+
+        }
+        return resultadoWord1&&resultadoWord2
+    }
+
 }
 
-private fun isAnagram(wordOne: String, wordTwo: String): Boolean {
-    if (wordOne.lowercase() == wordTwo.lowercase()) {
-        return false
-    }
-    return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
-}
+
