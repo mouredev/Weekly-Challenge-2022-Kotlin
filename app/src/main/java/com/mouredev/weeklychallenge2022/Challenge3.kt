2ff56271ca100a3f973f8b6ae38511f5f559ead0
AlmuLearn <65159503+AlmuLearn@users.noreply.github.com>
2022-01-21 20:50:15 +0100

    Update Challenge3.kt

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge3.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge3.kt
@@ -18,6 +18,50 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
-fun main() {
+	/**
+ * Vamos a indicar si un número es primo 
+ * vamos a suponer que los valores que se introducen son naturales 
+ */
+import kotlin.math.*
 
+fun main() {
+	
+    var n: Int = 104729
+    //llamamos a la función que comprueba si el número introducido es primo
+    if (primo(n)){
+        println("$n es primo")
+    }else{
+        println("$n no es primo")
+        }
+        
+    println("los números primos hasta $n son:")
+    for(i in 1..n){
+        if (primo(i)) println("$i")
+    }
 }
+
+private fun primo(num: Int): Boolean{
+    var cont = 0 //cuenta las veces que un número es divisible, es decir que da de resto 0
+    var raiz = sqrt(num.toDouble()).toInt() //pasamos el número introducido a Double para realizar la raiz cuadrada
+    //var raiz = sqrt(num1).toInt() //paso la raiz cuadrada a entero para poder usarla en el for
+    
+    if(num==2) return true //el 2 es el único número primo par
+    
+    if(num%2==0)return false //si el número es par, ya sabemos que no es primo
+    
+    //hacemos un recorrido desde el 1 hasta la raiz, debido a propiedades de los número primos
+    //que me indican que sólo he de probar si es divisible desde el 1 hasta la raiz de dicho número
+    //Si da de resto cero incrementamos el contador
+    for(i in 1..raiz){
+        if(num%i==0) cont++
+    }
+    
+    //Si el contador es menor de 2 significa que el número sólo ha sido divisible
+    //por dos números, es decir, entre 1 y entre si mismo, por lo que SI es primo
+    
+    if(cont<2) return true
+    else return false
+}
+	
+
+
