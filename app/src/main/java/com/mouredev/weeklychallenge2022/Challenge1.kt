e7a68b95150a4dd8d2ec7c1f6ecc5d6d8a0f0c36
Brais <lordprofesor@gmail.com>
2022-01-25 18:34:54 +0100

    -Reto 2 -Reto 3

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
@@ -21,12 +21,64 @@ package com.mouredev.weeklychallenge2022
  */
 
 fun main() {
+
+    //Test función de Moure
     println(isAnagram("amor", "roma"))
+
+    //Test función de Martinez
+    if(esAnagrama("canción         ","noi             cnac")){
+        println("SON ANAGRAMAS")
+
+    }else{
+        println("NO SON ANAGRAMAS")
+    }
 }
 
+/*
+FUNCIÓN DE MOURE
+ */
 private fun isAnagram(wordOne: String, wordTwo: String): Boolean {
     if (wordOne.lowercase() == wordTwo.lowercase()) {
         return false
     }
     return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
 }
+
+/*
+FUNCIÓN DE MARTINEZ
+ */
+fun contadorOcurrencias (palabra: String, letra: Char): Int {
+    return palabra.filter { it == letra }.length
+}
+
+//Función para dar formato a las distintas palabras
+fun formatearPalabra(palabra: String): String {
+    return palabra.lowercase().trim()
+        .replace(" ", "")
+        .replace("á","a")
+        .replace("é","e")
+        .replace("í","i")
+        .replace("ó","o")
+        .replace("ú","u")
+}
+
+//Función principal del reto
+fun esAnagrama (palabraUno : String, palabraDos : String) : Boolean{
+    val palabraUnoFormateada = formatearPalabra(palabraUno)
+    val palabraDosFormateada = formatearPalabra(palabraDos)
+
+    if((palabraUnoFormateada.length != palabraDosFormateada.length) || palabraUnoFormateada == palabraDosFormateada){
+        return false
+    }
+
+    for(letra in palabraUnoFormateada) {
+        val aparicionesEnPalabraUnoFormateada = contadorOcurrencias(palabraUnoFormateada, letra)
+        val aparicionesEnPalabraDosFormateada = contadorOcurrencias(palabraDosFormateada, letra)
+
+        if(aparicionesEnPalabraUnoFormateada != aparicionesEnPalabraDosFormateada){
+            return false
+        }
+    }
+    return true
+}
+
