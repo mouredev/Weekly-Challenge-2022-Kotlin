436d78ffbd5d2f3ffd84903c4b7349cb200749e4
Ferdinandpc <ferdinandpc@gmail.com>
2022-01-26 11:44:52 +0100

    Reto #4

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge4.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge4.kt
@@ -20,3 +20,30 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    val nada: Array<Int> = arrayOf()
+    val cuadrado: Array<Int> = arrayOf(2)
+    val rectangulo: Array<Int> = arrayOf(4, 2)
+    val triangulo: Array<Int> = arrayOf(3, 4, 5)
+    val pentagono: Array<Int> = arrayOf(3, 4, 5, 6, 7)
+    calculateArea(nada)
+    calculateArea(cuadrado)
+    calculateArea(rectangulo)
+    calculateArea(triangulo)
+    calculateArea(pentagono)
+}
+
+fun calculateArea(sides: Array<Int>) {
+    when (sides.size) {
+        0 -> println("Error, no hay figura")
+        1 -> println("El area del cuadrado es: " + (sides[0] * sides[0]))
+        2 -> println("El area del rectangulo es: " + (sides[0] * sides[1]))
+        3 -> {
+            val s = (sides[0] + sides[1] + sides[2]) / 2
+            val area = Math.sqrt((s * (s - sides[0]) * (s - sides[1]) * (s - sides[2])).toDouble())
+            println("El area del triangulo es: " + area)
+        }
+        else -> println("El calculo de ese poligono no esta contemplado")
+    }
+}
+
