9603106e145b4070b99569edb1cdc49073fddfcf
LordProfesor <lordprofesor@gmail.com>
2022-02-04 12:42:37 +0100

    -Reto 5 en proceso

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge5.kt
@@ -0,0 +1,28 @@
+package com.mouredev.weeklychallenge2022
+
+import android.graphics.BitmapFactory
+import androidx.core.graphics.get
+import com.squareup.picasso.Picasso
+import java.net.URL
+
+/*
+ * Reto #5
+ * ASPECT RATIO DE UNA IMAGEN
+ * Fecha publicación enunciado: 01/02/22
+ * Fecha publicación resolución: 07/02/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Crea un programa que se encargue de calcular y el aspect ratio de una imagen a partir de una url.
+ * - Url de ejemplo: https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png
+ * - Por ratio hacemos referencia por ejemplo a los "16:9" de una imagen de 1920*1080px.
+ */
+
+fun main() {
+    println("Hola")
+    val url = URL("https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png")
+    println("$url")
+    var bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+    //println("${bitmap.height}")
+
+
+}
