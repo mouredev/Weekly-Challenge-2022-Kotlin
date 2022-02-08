274168cc77d49b696e4f78de3a3b2161b2543861
icordondominguez <icordondominguez@gmail.com>
2022-02-08 13:12:32 +0100

    initial commit

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -21,17 +21,4 @@ package com.mouredev.weeklychallenge2022
 
 fun main() {
 
-    for (index in 1..100) {
-        val divisibleByThree = index % 3 == 0
-        val divisibleByFive = index % 5 == 0
-        if (divisibleByThree && divisibleByFive) {
-            println("fizzbuzz")
-        } else if (divisibleByThree) {
-            println("fizz")
-        } else if (divisibleByFive) {
-            println("buzz")
-        } else {
-            println(index)
-        }
-    }
 }
