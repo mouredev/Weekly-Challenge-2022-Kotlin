7b6bab985c65afdbadcabe297f482ce530d440cc
Jose Angel Mendez Santiago <jamsmendez02@gmail.com>
2022-02-11 22:06:47 -0600

    Reto 6

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge6.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge6.kt
@@ -18,3 +18,65 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+  var res = ownReverse("racecar")
+  println(res)
+
+  res = ownReverse("robot")
+  println(res)
+
+  res = ownReverse("Ramen")
+  println(res)
+
+  res = ownReverse("I'm hungry!")
+  println(res)
+
+  res = ownReverse("drawer")
+  println(res)
+
+  res = ownReverse("子猫")
+  println(res)
+
+  res = ownReverse("uüu")
+  println(res)
+
+  res = ownReverse("εγγραφή")
+  println(res)
+}
+
+fun ownReverse(s: String): String {
+  if (s.isEmpty()) return ""
+
+  val chars = s.toCharArray()
+  val size = chars.size
+
+  val reverseInput = CharArray(size)
+  var start = size
+
+  var i = 0
+  while (i < size) {
+    var j = i + 1
+
+    while (j < size) {
+      val code = chars[j].category.value
+      val charCategory = CharCategory.valueOf(code)
+
+      val isSafe = CharCategory.COMBINING_SPACING_MARK == charCategory ||
+          CharCategory.NON_SPACING_MARK == charCategory ||
+          CharCategory.ENCLOSING_MARK == charCategory
+
+      if (!isSafe) break
+
+      if (isSafe) j += 1
+    }
+
+    for (k in (j - 1) downTo i) {
+      start -= 1
+      reverseInput[start] = chars[k]
+    }
+
+    i = j
+  }
+
+  return String(reverseInput)
+}
