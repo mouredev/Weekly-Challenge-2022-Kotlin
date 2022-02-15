a9a10df3018facd99839dcfd24a5962f2b742c09
Eduardo Binns <eduardo.binns.sanchez@gmail.com>
2022-02-15 13:49:38 -0600

    finish challenge7

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
@@ -19,3 +19,28 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+
+fun main(){
+
+    wordsCount( "so we created this collection of online text tools. Unlike many other tools, we made our tools free")
+}
+
+fun wordsCount(text: String){
+    val re = "[^A-Za-z0-9 ]".toRegex()
+    val splitText = re.replace(text, "").lowercase().split(" ")
+
+    for (i in splitText.indices){
+        var countWord = 0
+        val actualWord = splitText[i]
+        for (j  in splitText.indices){
+            val word = splitText[j]
+            if ((word == actualWord)){
+                countWord +=1
+            }
+        }
+
+        println("la palabra $actualWord sale $countWord")
+        countWord = 0
+    }
+}
