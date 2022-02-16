9c4d1448e171f8b91204966227fe6d3423603cd3
icedrek <icedrek@gmail.com>
2022-02-16 17:45:38 +0100

    v0.1

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
@@ -19,3 +19,58 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+
+fun main() {
+   val inputText = "Hola mundo en Kotlin en"
+   
+   countWords(inputText)
+}
+
+private fun countWords (inputText: String) {
+   var counter = ArrayList<Pair<String, Int>> ()
+   var stringAux = "" //Para no usar substring
+   // var index = 0
+    var maxIndex = inputText.length - 1
+    val puntuationSigns = arrayOf(" ", ",", ".", ":", ";")
+   
+   for (index in 0..maxIndex) {
+      if (index==maxIndex&&inputText[index].toString() !in puntuationSigns) stringAux += inputText[index]
+       
+      if (index==maxIndex||inputText[index].toString() in puntuationSigns){
+            var sile = chekWord(stringAux, counter)
+            if (!sile.first){
+                val pair = Pair(stringAux, 1)
+                counter.add(pair)            
+            } else {                
+                counter[sile.second].second++
+            }
+            
+            stringAux=""
+        } else {
+            stringAux += inputText[index] 
+        }
+   }
+   
+   if (chekWord("Kotlin", counter).first) println("si le")
+
+   println(counter)
+   
+}
+
+private fun isPuntuation (inputChar: String): Boolean {
+   var check = false
+   
+   if (inputChar in arrayOf(" ", ",", ".", ":", ";")) check = true
+   
+   return check
+}
+
+private fun chekWord(inputWord:String, wordCounter:ArrayList<Pair<String, Int>>): Pair<Boolean, Int> {
+   var exists = Pair(false, 0)
+
+   for (i in 0..wordCounter.size - 1) {
+      if (inputWord in wordCounter[i].first) exists = Pair(true, i)
+   }
+   return exists
+}
