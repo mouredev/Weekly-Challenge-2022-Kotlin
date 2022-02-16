e9dff2a03c11a884bdfd7a60cac192f5f4a10a7b
Dani <alegandro2507@gmail.com>
2022-02-16 19:02:22 +0100

    Solucion Reto #7

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
@@ -19,3 +19,29 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main() {
+    println(counter("Hola Mundo.!,"))
+    println(counter(testText()))
+}
+
+private fun counter(text: String) : String {
+    val re = "[^A-Za-z0-9 ]".toRegex()
+    val textAux = re.replace(text, "").lowercase().split(" ")
+
+    val mapWorld = mutableMapOf<String, Int>()
+
+    for (world in textAux) {
+        if (mapWorld.containsKey(world)){
+            mapWorld[world] = mapWorld[world]!! + 1
+        }else{
+            mapWorld[world] = 1
+        }
+    }
+
+    return mapWorld.toString()
+}
+
+private fun testText() : String {
+    return "Kotlin es un lenguaje de programación de tipado estático que corre sobre la máquina virtual de Java y que también puede ser compilado a código fuente de JavaScript. Es desarrollado principalmente por JetBrains en sus oficinas de San Petersburgo (Rusia). El nombre proviene de la isla de Kotlin, situada cerca de San Petersburgo. Kotlin fue nombrado lenguaje del mes por la revista Dr. Dobb' Journal en su edición de enero de 2016. Aunque no tiene una sintaxis compatible con Java, Kotlin está diseñado para interoperar con código Java y es dependiente del código Java de su biblioteca de clases, tal como pueda ser el entorno de colecciones de Java (Java collections framework). Kotlin ha sido conocido por ser utilizado en la creación de aplicaciones de Android."
+}
