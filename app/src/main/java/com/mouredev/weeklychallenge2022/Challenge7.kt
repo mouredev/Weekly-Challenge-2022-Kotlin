390510f174b63d91ecf973f66882480555370e5d
Bruce V <brunoveg@outlook.com.ar>
2022-02-20 13:54:45 -0300

    Solución Reto #7
    Contar palabras de una frase

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
@@ -19,3 +19,48 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+fun main() {
+    val text = "¿Quién habría dejado un baúl en medio de la calle...? " +
+            "Miles de pensamientos le vinieron a la cabeza: \"Me pregunto si se lo " +
+            "habrán olvidado o lo habrán abandonado; no sé si lo estarán buscando o ya lo darán " +
+            "por perdido. ¡MUERO POR SABER QUE HAY DENTRO DEL BAÚL!\". " +
+            "Después de mucho meditar decidió seguir camino " +
+            "(haría de cuenta que nunca lo había visto)."
+
+    println("Frase a Analizar:\n$text\n")
+    println("Listado de Palabras encontradas:")
+    countWords(text)
+}
+
+private fun countWords(text: String) {
+
+    var cleanText = ""
+    for (index in 0 until text.length) {
+        if (!checkPuntuationMarks(text[index].toString())) {
+            cleanText += text[index]
+        }
+    }
+
+    var wordsArray = cleanText.lowercase().split(" ").sorted()
+    var previousWord = wordsArray[0]
+    var countWord = 0
+
+    for (index in wordsArray.indices) {
+        if (previousWord == wordsArray[index]) {
+            countWord++
+        }
+        else {
+            println("$previousWord -> $countWord")
+            previousWord = wordsArray[index]
+            countWord = 1
+        }
+    }
+    println("$previousWord -> $countWord")
+}
+
+private fun checkPuntuationMarks(char: String) : Boolean {
+    return when (char) {
+        ".", ",", ";", ":", "¿", "?", "¡", "!", "(", ")", "\"" -> true
+        else -> false
+    }
+}
