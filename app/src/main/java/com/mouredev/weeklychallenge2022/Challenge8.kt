390e13afd2fa0c7e65a047ad62d28753c7b23cc6
Dani <alegandro2507@gmail.com>
2022-02-24 11:33:42 +0100

    Solución Reto #8

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge8.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge8.kt
@@ -1,5 +1,8 @@
 package com.mouredev.weeklychallenge2022
 
+import android.icu.number.IntegerWidth
+import kotlin.math.pow
+
 /*
  * Reto #8
  * DECIMAL A BINARIO
@@ -16,4 +19,37 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main() {
+    var decimal = 1023
+
+    println(convert_manual(decimal))
+    println(convert_auto(decimal))
+    println(convert_manual_recursivo(decimal,  0, 1))
+}
+
+private fun convert_manual(decimal: Int) : Long {
+    var decimalAux = decimal
+    var binary: Long = 0
+    var count = 1
+
+    while (decimalAux != 0) {
+        binary += ((decimalAux % 2) * count).toLong()
+        decimalAux /= 2
+        count *= 10
+    }
+
+    return binary
+}
+
+private fun convert_manual_recursivo(decimal: Int, binary: Long, count: Int) : String {
+    return if (decimal != 0)
+        convert_manual_recursivo(decimal / 2, binary + ((decimal % 2) * count).toLong(), count * 10)
+    else
+        binary.toString()
+}
+
+private fun convert_auto(decimal: Int) : String {
+    return Integer.toBinaryString(decimal)
+}
 
