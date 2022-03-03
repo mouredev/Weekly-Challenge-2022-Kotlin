35bf8d60cc89ce37a242f633d7779061adbe5a3a
Oscar Bravo <oscar.bravo@usach.cl>
2022-03-03 12:37:47 -0300

    Python|Solución Reto #9
    Add Translate from Morse Code Function.

--- Python/Challenge9.py
+++ Python/Challenge9.py
@@ -66,11 +66,11 @@ MORSE_DICT = {
 }
 
 
-def translate_to_morse(phrase: str):
+def translate_to_morse(natural_phrase: str):
     # String a imprimir
     translated_phrase = ""
 
-    for letter in phrase.lower():
+    for letter in natural_phrase.lower():
 
         # Obtiene la traducción de la letra a través del diccionario Morse, si no existe o no se encuentra retorna None
         letter_morse = MORSE_DICT.get(letter)
@@ -88,13 +88,67 @@ def translate_to_morse(phrase: str):
             translated_phrase += f"{letter_morse} "
 
     # Imprime la frase en Código Morse
-    print(f"The phrase '{phrase}' in morse is: {translated_phrase}")
+    print(f"The phrase '{natural_phrase}' in morse is: '{translated_phrase}'.")
+
+
+def translate_to_natural_text(morse_phrase: str):
+    # String a imprimir
+    translated_phrase = ""
+
+    # Del diccionario de Texto Natural a Código Morse, se crea un nuevo diccionario,
+    # que es de Código Morse a Texto Natural
+    latin_dict = {value: key for key, value in MORSE_DICT.items()}
+
+    # Separa lo transmitido en morse, en palabras únicas.
+    for word in morse_phrase.split("   "):
+        # Cada palabra es separado
+        for letter_morse in word.split():
+
+            # Intenta obtener la letra del diccionario creado
+            latin_letter = latin_dict.get(letter_morse)
+
+            # Si se tiene un error transmitido o no existe la letra, lo marca con un *
+            if letter_morse == "....--.....---..-...-.." or latin_letter is None:
+                translated_phrase += "*"
+
+            # En caso contrario, añade la letra traducida desde
+            else:
+                translated_phrase += latin_letter
+
+        # Añade un espacio al final de la palabra
+        translated_phrase += " "
+
+    # Imprime la frase en Código Morse
+    print(f"The Morse Code '{morse_phrase}' in natural text is: '{translated_phrase}'.")
+
+
+def morse_translator(phrase: str):
+
+    is_morse = False
+
+    # Si alguna de las letras de la frase, está en los Valores del diccionario, lo traducirá a Texto Natural
+    # En caso contrario, lo traduce a Morse.
+    for letter in phrase.lower().split():
+
+        # Si la letra se encuentra en los valores del diccionario de morse, deja la traducción a Morse
+        if letter in MORSE_DICT.values():
+            is_morse = True
+
+    if is_morse:
+        translate_to_natural_text(phrase)
+
+    else:
+        translate_to_morse(phrase.lower())
 
 
 # Test
-translate_to_morse("SOS")
+morse_translator("Hola Mundo 1/3")
+morse_translator("Olé")
+morse_translator("··· --- ···")
+morse_translator("-- --- ··- ·-· · -·· · ···-")
 
 # Result Test
-# The phrase 'Hola Mundo 1/3' in morse is: ···· --- ·-·· ·-   -- ··- -· -·· ---   ·---- -··-· ···--
-# The phrase 'Olé' in morse is: --- ·-·· ....--.....---..-...-..
-# The phrase 'SOS' in morse is: ··· --- ···
+# The phrase 'hola mundo 1/3' in morse is: '···· --- ·-·· ·-   -- ··- -· -·· ---   ·---- -··-· ···-- '.
+# The phrase 'olé' in morse is: '--- ·-·· ....--.....---..-...-.. '.
+# The Morse Code '··· --- ···' in natural text is: 'sos '.
+# The Morse Code '-- --- ··- ·-· · -·· · ···-' in natural text is: 'mouredev '.
