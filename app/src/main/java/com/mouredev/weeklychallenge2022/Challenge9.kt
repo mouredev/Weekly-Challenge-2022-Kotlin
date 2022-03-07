12c30b1cb84e693516a2d3e3ef870c52b74d8b05
Miguel Ángel Delgado <migue@miguelandelgado.com>
2022-03-07 13:39:00 +0100

    Solución Reto #9

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge9.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge9.kt
@@ -20,3 +20,86 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    val naturalText = "Esto es una prueba en lenguaje natural"
+    val morseText = ".... .- ...  .-. . -.-. .. -... .. -.. ---  . ... - --- ..--.."
+
+    println(EvaluateString(naturalText))
+    println(EvaluateString(morseText))
+}
+
+private fun EvaluateString(string: String): String {
+    // Esta función detecta el contenido de la cadena pasada y en función de ello, codifica o decodifica
+
+    var resultString = ""
+    val regex = Regex(pattern = "[A-Z]")
+    val dict = hashMapOf (".-" to "A", "-..." to "B", "-.-." to "C", "----" to "CH", "-.." to "D", "." to "E",
+                          "..-." to "F", "--." to "G", "...." to "H", ".." to "I", ".---" to "J", "-.-" to "K",
+                          ".-.." to "L", "--" to "M", "-." to "N", "---" to "O", ".--." to "P", "--.-" to "Q",
+                          ".-." to "R", "..." to "S", "-" to "T", "..-" to "U", "...-" to "V", ".--" to "W",
+                          "-..-" to "X", "-.--" to "Y", "--.." to "Z", "-----" to "0", ".----" to "1",
+                          "..---" to "2", "...--" to "3", "....-" to "4", "....." to "5", "-...." to "6",
+                          "--..." to "7", "---.." to "8", "----." to "9", ".-.-.-" to ".", "--..--" to ",",
+                          "..--.." to "?", ".-..-." to "\"", "-..-. " to "/")
+
+    if (regex.containsMatchIn(input= string.uppercase()))
+        resultString = TextToMorse(string, dict)
+    else
+        resultString = MorseToText(string, dict)
+
+    return resultString
+}
+
+private fun TextToMorse (string: String, dict: HashMap<String, String>): String {
+
+    var morseString = ""
+    var step = false
+    val reversedDict = dict.entries.associate{(k,v)-> v to k}
+
+    string.forEachIndexed{index, element ->
+        if (step){
+            step = false
+        }
+        else {
+            if (element in " ")
+                morseString += " "
+            else if (element.uppercase().equals("C")) { // Comprobacion para la CH
+                if ((string[index + 1].uppercase().equals("H")) and (index + 1 < string.length )) {
+                    // La siguiente letra es una H, asi que hay que poner el caracter CH
+                    morseString += reversedDict.getValue("CH") + " "
+                    step = true
+                }
+                else
+                    morseString += reversedDict.getValue("C") + " "
+            }
+            else
+                morseString += reversedDict.getValue(element.toString().uppercase()) + " "
+        }
+    }
+
+    return morseString
+}
+
+private fun MorseToText (string: String, dict: HashMap<String, String>): String {
+
+    var naturalString = ""
+    var word = ""
+
+    string.forEachIndexed{index, element ->
+        if (element in " ") {
+            if ((string[index + 1] in " ") and (index + 1 < string.length ))
+                naturalString += dict.getValue(word) + " "
+            else
+                if (word !in " ")
+                    naturalString += dict.getValue(word)
+                else
+                    naturalString += " "
+            word = ""
+        }
+        else
+            word += element
+    }
+    naturalString += dict.getValue(word)
+
+    return naturalString
+}
