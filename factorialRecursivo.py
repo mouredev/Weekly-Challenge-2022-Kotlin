45034a4a291e613d1cf537c1b34cccefc96e2de7
Grumilo <102401963+Grumilo@users.noreply.github.com>
2022-03-29 22:47:40 +0200

    Solución Reto #13 Python

---
+++ factorialRecursivo.py
@@ -0,0 +1,13 @@
+
+def calFactorial(numero):
+    
+    if numero == 1:
+        return 1
+    else:
+        return numero * calFactorial(numero-1)
+
+#Llamada función principal
+if __name__ == '__main__':
+    numero = int(input('Introduce un número: '))
+    calFactorial(numero)
+    print(f'El factorial de {numero} es:',calFactorial(numero))
