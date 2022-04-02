d3e2534d27a63821b7753496da5897228f24f455
Felix Escandon <96452288+Fenixlix@users.noreply.github.com>
2022-04-02 15:10:53 -0500

    Solution Challenge#13

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge13.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge13.kt
@@ -16,3 +16,25 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main(){
+    var number = 5
+    println("\nThe factorial of $number is: ${getFactorial(number)}")
+    number = -5
+    println("\nThe factorial of $number is: ${getFactorial(number)}")
+}
+
+private fun getFactorial(number : Int): Int{
+    if (number < 0){
+        println("\nWrong number, only positive integers")
+        return 0
+    }
+    return if( number > 1){
+        val num = getFactorial(number-1)
+        print("|$num x $number|")
+        number * num
+    } else {
+        print("|$number|")
+        number
+    }
+}
