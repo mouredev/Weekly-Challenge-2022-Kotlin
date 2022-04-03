f67399df54ffc812fc32f8fdbda9a13f53f7918f
grubhart <grubhart@gmail.com>
2022-04-03 14:48:28 -0500

    refactorizamos retirar if

--- src/main/java/dev/gruhart/factorial/CalculaFactorial.java
+++ src/main/java/dev/gruhart/factorial/CalculaFactorial.java
@@ -3,10 +3,9 @@ package dev.gruhart.factorial;
 public class CalculaFactorial {
 
     public int calcula(int i) {
-        if (i>1) {
-            while(i > 1){
-                return i * calcula(i-1);
-            }
+
+        while(i > 1){
+            return i * calcula(i-1);
         }
         return 1;
     }
