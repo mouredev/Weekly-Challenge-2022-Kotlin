ed5a3bb2736e879c6ca98acaf7ad59e3d1a2ead0
Oscar Parada <oscar.parada@cinfo.es>
2022-04-12 18:35:13 +0200

    Challenge15 Solution

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge15.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge15.kt
@@ -1,5 +1,17 @@
 package com.mouredev.weeklychallenge2022
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.time.format.ResolverStyle
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.math.abs
+
 /*
  * Reto #15
  * ¿CUÁNTOS DÍAS?
@@ -21,3 +33,33 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+@RequiresApi(Build.VERSION_CODES.O)
+fun main() {
+
+
+     println(cuantosDias("25/12/2020", "20/12/2020")) //5
+     println(cuantosDias("15/10/2022", "18/10/2022")) //3
+     println(cuantosDias("31/11/2020", "17/11/2021")) //error (-1)
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@SuppressLint("SimpleDateFormat")
+fun cuantosDias(date1: String, date2: String): Int {
+
+     val DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/uuuu")
+
+     try {
+          LocalDate.parse(date1, DATE_FORMATTER.withResolverStyle(ResolverStyle.STRICT))
+          LocalDate.parse(date2, DATE_FORMATTER.withResolverStyle(ResolverStyle.STRICT))
+     } catch (e: DateTimeParseException) {
+          println("Fechas incorrectas: " + e.message)
+          return -1
+     }
+     val firstDate: Date = SimpleDateFormat("dd/MM/yyyy").parse(date1) as Date
+     val secondDate: Date = SimpleDateFormat("dd/MM/yyyy").parse(date2) as Date
+
+     return TimeUnit.MILLISECONDS.toDays(abs(firstDate.time - secondDate.time)).toInt()
+}
+
+//By Osovankt
