ac47677caa6e6dd541638a6a6a36bcffcca7063b
yatusaetec2004 <71166741+yatusaetec2004@users.noreply.github.com>
2022-05-09 15:57:34 -0500

    RETO 19 KOTLIN

--- app/src/main/java/com/mouredev/weeklychallenge2022/Reto19.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Reto19.kt
@@ -3,7 +3,7 @@
  * CONVERSOR TIEMPO
  * Fecha publicación enunciado: 09/05/22
  * Fecha publicación resolución: 16/05/22
- * Dificultad: FACIL
+ * Dificultad: REGALADO
  *
  * Enunciado: Crea una función que reciba días, horas, minutos y segundos (como enteros) y retorne su resultado en milisegundos.
  */
