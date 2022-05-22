285eb8d7c84ed8c998f968d286ee0d519da623d3
Miguel Ángel Delgado <migue@miguelandelgado.com>
2022-05-22 12:04:41 +0200

    Solución Reto #20

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge20.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge20.kt
@@ -1,5 +1,6 @@
 package com.mouredev.weeklychallenge2022
 
+import kotlinx.coroutines.*
 /*
  * Reto #20
  * PARANDO EL TIEMPO
@@ -18,3 +19,24 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+suspend fun main() {
+
+    coroutineScope {
+        launch{Addition(1, 1, 10L, 1)}
+        launch{Addition(4, 3, 2L, 2)}
+        launch{Addition(3, 5, 5L, 3)}
+        launch{Addition(5, 1, 3L, 4)}
+        launch{Addition(-1, 10, 8L, 5)}
+        launch{Addition(20, -12, 7L, 6)}
+        launch{Addition(-10, -10, 1L, 7)}
+    }
+}
+
+private suspend fun Addition(numberOne: Int, numberTwo: Int, seconds: Long, count: Int) {
+    CoroutineScope(Dispatchers.IO).launch {
+        delay(seconds*1000)
+    }.join()
+    println("El resultado de la suma  $count es: ${numberOne + numberTwo}")
+}
+
