82d2f677f73e0fd3fa03115c5ee0878f0991573e
miguel <miguel.perez@juanxxiii.net>
2022-05-29 23:58:04 +0200

    Control de numeros negativos y comentarios en codigo

--- Reto21/README.md
+++ Reto21/README.md
@@ -3,7 +3,7 @@
  * Fecha publicación enunciado: 23/05/22
  * Fecha publicación resolución: 01/06/22
  * Dificultad: MEDIA
- *
+ 
  * Enunciado: Lee el fichero "Challenge21.txt" incluido en el proyecto, calcula su resultado e imprímelo.
  * - El .txt se corresponde con las entradas de una calculadora.
  * - Cada línea tendrá un número o una operación representada por un símbolo (alternando ambos).
--- Reto21/calculadora.py
+++ Reto21/calculadora.py
@@ -1,42 +1,60 @@
-import os
+from cmath import nan
 import sys
 
 def main():
-    myFile ='Challenge21.txt'
+    myFile ='Reto21/Challenge21.txt'
     #myFile = input("Introduce ruta del fichero: ")
 
+    # Leo fichero, almaceno las líneas en una lista de strings y cierro el fichero
     fichero = open(myFile, 'r')
     Lines = fichero.readlines()
     fichero.close()
-    #print(len(fichero.readlines()))
+    
+    # Variable para almacenar el resultado
     result = 0
+    # Variable para almacenar una operación matemática
     data = ''
+    # Variable contador
     lineNum = 1
+    # Recorro las líneas del fichero
     for line in Lines: 
+        # Concateno línea en variable data
         data += line.strip()
+        # Por cada 3 líneas tengo una operación matemática
         if (lineNum % 3 == 0):
-            print(data)
+            #print(data)
+            # Almaceno el resultado de la operación matemática
             result = getResult(data)
+            # Incluyo el resultado como un elemento más de la lista
             Lines.insert(lineNum,str(result))
+            # Vacío data para que sólo tenga una operación matemática
             data=''
         lineNum += 1
-    print(result)    
+    # Redondeo el resultado a 2 decimales
+    print(round(result,2))    
         
 
 def getResult(operation):
     
     if(operation.count('+') > 0):
         num1,num2 = operation.split('+')
-        return round(castNumber(num1)+castNumber(num2),2)
+        return (castNumber(num1)+castNumber(num2))
     elif(operation.count('-')):
-        num1,num2 = operation.split('-')
-        return round(castNumber(num1)-castNumber(num2),2)
+        elems = operation.split('-')
+        # Para control de números negativos
+        if len(elems) != 2:
+            ind = operation.find('-',1)
+            num1 = operation[0:ind]
+            num2 = operation[ind+1:]
+        else:
+           num1,num2 = operation.split('-')
+        return (castNumber(num1)-castNumber(num2))
     elif(operation.count('*')):
         num1,num2 = operation.split('*')
-        return round(castNumber(num1)*castNumber(num2),2)
+        return (castNumber(num1)*castNumber(num2))
     elif(operation.count('/')):
         num1,num2 = operation.split('/')
-        return round(castNumber(num1)/castNumber(num2),2)
+        return (castNumber(num1)/castNumber(num2))
     else:
         sys.exit(f'No se han podido resolver las operaciones. Error en: {operation}')
 
