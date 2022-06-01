ed3af9a5bf3638822a5699ef9d17542feebe4092
Brais Moure <braismoure@mouredev.com>
2022-06-01 22:03:34 +0200

    Resolución Reto #21 y enunciado Reto #22

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge22.kt
@@ -0,0 +1,21 @@
+package com.mouredev.weeklychallenge2022
+
+/*
+ * Reto #22
+ * CONJUNTOS
+ * Fecha publicación enunciado: 01/06/22
+ * Fecha publicación resolución: 07/06/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea una función que reciba dos array, un booleano y retorne un array.
+ * - Si el booleano es verdadero buscará y retornará los elementos comunes de los dos array.
+ * - Si el booleano es falso buscará y retornará los elementos no comunes de los dos array.
+ * - No se pueden utilizar operaciones del lenguaje que lo resuelvan directamente.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
