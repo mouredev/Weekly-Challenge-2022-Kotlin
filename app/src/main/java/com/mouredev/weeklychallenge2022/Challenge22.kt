b2d60fefcc0d5ef07cdfb73ad06c9eb2ca03ca80
malopezrom <michayita@gmail.com>
2022-06-07 00:29:36 +0200

    Solucion Reto#22 Kotlin

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge22.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge22.kt
@@ -19,3 +19,79 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+/**
+ * Funcion Principal
+ */
+fun main(){
+    print("Elementos comunes: " )
+    printArray(checkElements(arrayOf(3, 1,"ff","a"), arrayOf(3, 4, 3, "a", 7), true))
+    print("Elementos NO comunes: " )
+    printArray(checkElements(arrayOf("a", "b", "c", "d", "e"), arrayOf("a", "b"), false))
+
+}
+
+/**
+ * Funcion que imprime por pantalla un array
+ * @param array Array a imprimir
+ */
+private fun printArray(array: Array<Any>) {
+
+    for (i in array.indices) {
+        print("${array[i]} ")
+    }
+    println()
+}
+
+/**
+ * Funcion que devuelve los elementos comununes o no comunes de dos arrays
+ * @param arrayOne Array de elementos 1
+ * @param arrayTwo Array de elementos 2
+ * @param commonElements Booleano que indica si se quiere los elementos comunes o no comunes
+ * @return Array de elementos comunes o no comunes
+ */
+inline fun <reified T> checkElements(arrayOne: Array<T>, arrayTwo: Array<T>, commonElements: Boolean): Array<T> {
+    val commonArray = mutableListOf<T>()
+    val notCommonArray = mutableListOf<T>()
+
+    arrayOne.forEach{
+        if(arrayTwo.myContains(it)){
+                commonArray.addElement(it)
+            }else
+                notCommonArray.addElement(it)
+        }
+
+    return if(commonElements){
+        commonArray.toTypedArray()
+    }else notCommonArray.toTypedArray()
+
+
+}
+
+/**
+ * Funcion de extension que añade un elemento a una lista pero si ya existe no lo añade
+ * @param element Elemento a añadir
+ * @return Booleano que indica si se ha añadido o no el elemento
+ */
+fun <T> MutableList<T>.addElement(element: T): Boolean {
+        if(!this.contains(element)){
+            this.add(element)
+            return true
+        }
+        return false
+}
+
+/**
+ * Funcion de extension que comprueba si un elemento esta en un array
+ * @param value Elemento a comprobar
+ * @return Booleano que indica si el elemento esta en el array
+ */
+fun <T>Array<T>.myContains(value:T):Boolean{
+    this.forEach {
+        if(it == value){
+            return true
+        }
+    }
+    return false
+}
+
