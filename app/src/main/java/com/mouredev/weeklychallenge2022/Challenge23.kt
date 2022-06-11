e6bf92c58d7915ebe9a283adcf29f6382ca31cd9
Ferdinandpc <ferdinandpc@gmail.com>
2022-06-11 23:25:02 +0200

    Reto 23

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge23.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge23.kt
@@ -17,3 +17,32 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main() {
+    val num1 = 10
+    val num2 = 15
+    println("MCD de $num1 y $num2 es " + mcdCalculator(num1, num2))
+    println("MCD de $num1 y $num2 recursivo es " + mcdCalculatorRecursive(num1, num2))
+    println("MCM de $num1 y $num2 es " + mcmCalculator(num1, num2))
+}
+
+fun mcdCalculatorRecursive(num1: Int, num2: Int): Int {
+    if (num2 == 0) return num1
+    return mcdCalculatorRecursive(num2, num1 % num2);
+}
+
+fun mcdCalculator(num1: Int, num2: Int): Int {
+    var aux1: Int = num1
+    var aux2: Int = num2
+    var temp: Int
+    while (aux2 > 0) {
+        temp = aux2
+        aux2 = aux1 % aux2
+        aux1 = temp
+    }
+    return aux1
+}
+
+fun mcmCalculator(a: Int, b: Int): Int {
+    return a * b / mcdCalculator(a, b)
+}
