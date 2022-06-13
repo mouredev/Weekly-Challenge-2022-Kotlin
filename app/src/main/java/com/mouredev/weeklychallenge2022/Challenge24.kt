6101c07637b2857dc603b0c510d3bf5f3996b37a
Felix Escandon <96452288+Fenixlix@users.noreply.github.com>
2022-06-13 17:08:48 -0500

    Solution Challenge#24

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge24.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge24.kt
@@ -16,3 +16,56 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+// The gear functions are in reference to One Piece
+fun main(){
+    gearOne()
+    gearTwo()
+    gearThree()
+    gearFourth()
+    gearFive()
+}
+
+private fun gearOne(){
+    for(x in 1..100){ print("$x ")}
+    println()
+}
+
+private fun gearTwo(){
+    var x = 1
+    while (x <=100){
+        print("${x++} ")
+    }
+    println()
+}
+
+private fun gearThree(){
+    var x = 1
+    repeat(100){
+        print("${x++} ")
+    }
+    println()
+}
+
+// It is not the best practice but it is a little funnier
+private fun gearFourth(){
+    var x = 1
+    var YaLlegamos = false
+    while (YaLlegamos != true){
+        if (x<=100){
+            print("${x++} ")
+        } else {
+            YaLlegamos = true
+        }
+    }
+    println()
+}
+
+// I haven't watch the latest episodes, I don't know if it gets to have a five transformation
+private fun gearFive(){
+    var x = 1
+    do {
+        print("${x++} ")
+    } while (x<=100)
+    println()
+}
