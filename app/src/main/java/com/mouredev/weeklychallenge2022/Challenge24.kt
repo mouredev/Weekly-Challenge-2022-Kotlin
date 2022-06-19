86b02f8862adceb6a77a4541ed592bbe88508868
Arturo Romero Sánchez <91941978+arrrturo@users.noreply.github.com>
2022-06-19 12:38:26 +0200

    Update Challenge24.kt

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge24.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge24.kt
@@ -16,3 +16,86 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main(){
+   // firstCase()
+   // secondCase()
+   // thirdCase()
+   // fourthCase()
+   // fifthCase()
+   // sixthCase()
+}
+
+private fun firstCase(){
+    // con un for de toda la vida
+    for(i in 1..100){
+        println(i)
+    }
+}
+
+private fun secondCase(){
+    // con un bucle while
+    var i = 1
+
+    while(i <= 100){
+        println(i)
+        i += 1
+    }
+}
+
+private fun thirdCase(){
+    // bucle do-while
+    var i = 1
+
+    do{
+        println(i)
+        i += 1
+    }while(i <= 100)
+}
+
+private fun fourthCase(){
+    // for para agregar los numeros a una lista y despues un for para imprimir cada numero de esa lista
+    var lista = arrayListOf(1)
+
+    for(i in 2..100){
+        lista.add(i)
+    }
+
+    for(i in lista){
+        println(i)
+    }
+}
+
+private fun fifthCase(){
+    // for para agregar los numeros a una lista de forma decreciente y despues un for para imprimir cada numero de esa lista revertida
+    var lista = arrayListOf(100)
+
+    for(i in 99 downTo 1){
+        lista.add(i)
+    }
+
+    for(i in lista.reversed()){
+        println(i)
+    }
+}
+
+private fun sixthCase(){
+    // de 1 a 100 crear un array de numeros impares y pares, posterior mente unir ambos y ordenarlos, recorrer el array e imprimir cada valor en él
+    val even = arrayListOf<Int>()
+    val odd = arrayListOf<Int>()
+    for(i in 1..100){
+        if(i % 2 == 0){
+            even.add(i)
+        }
+        if(i % 2 != 0){
+            odd.add(i)
+        }
+    }
+
+    odd.addAll(even)
+    odd.sort()
+
+    for(i in odd){
+        println(i)
+    }
+}
