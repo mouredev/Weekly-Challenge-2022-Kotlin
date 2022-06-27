26ab7d072027120a10958427d6455efc584baf97
icedrek <icedrek@gmail.com>
2022-06-27 20:14:12 +0200

    Challenge25

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge25.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge25.kt
@@ -20,3 +20,45 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+ fun main() {
+    // Values: Rock(R), Scissors(S), Paper(P)
+    // Rules -> R wins S ; S wins P; P wins R
+    val play1 = mapOf('R' to 'R') // tie
+    val play2 = mapOf('R' to 'S') // Player 1
+    val play3 = mapOf('R' to 'P') // Player 2
+    val play4 = mapOf('S' to 'R') // Player 2
+    val play5 = mapOf('S' to 'S') // tie
+    val play6 = mapOf('S' to 'P') // Player 1
+    val play7 = mapOf('P' to 'R') // Player 1
+    val play8 = mapOf('P' to 'S') // Player 2
+    val play9 = mapOf('P' to 'P') // tie
+
+    val playsList = listOf(play2, play3, play6)
+
+    println(checkRPSResult(playsList))
+}
+
+private fun checkRPSResult(playsList: List<Map<Char, Char>>): String {
+    var counter = 0
+
+    playsList.forEach {
+        if (it.containsKey('R')) {
+            if (it.containsValue('S')) counter--
+            else if (it.containsValue('P')) counter++
+        }
+        if (it.containsKey('S')) {
+            if (it.containsValue('P')) counter--
+            else if (it.containsValue('R')) counter++
+        }
+        if (it.containsKey('P')) {
+            if (it.containsValue('R')) counter--
+            else if (it.containsValue('S')) counter++
+        }
+    }
+
+    if (counter < 0) return "Winner Player 1"
+    if (counter > 0) return "Winner Player 2"
+
+    return "Tie"
+}
