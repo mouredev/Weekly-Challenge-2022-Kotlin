e513b0518e5acecc3642c7122b5e882b27fc2b0d
Álvaro <alvarora12@gmail.com>
2022-07-11 13:55:58 +0200

    ch27

---
+++ solutions/js/challenges/Challenge26.js
@@ -0,0 +1,18 @@
+/*
+ * Reto #26
+ * CUADRADO Y TRIÁNGULO 2D
+ * Fecha publicación enunciado: 27/06/22
+ * Fecha publicación resolución: 07/07/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que dibuje un cuadrado o un triángulo con asteriscos "*".
+ * - Indicaremos el tamaño del lado y si la figura a dibujar es una u otra.
+ * - EXTRA: ¿Eres capaz de dibujar más figuras?
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
---
+++ solutions/js/challenges/Challenge27.js
@@ -0,0 +1,22 @@
+/*
+ * Reto #27
+ * VECTORES ORTOGONALES
+ * Fecha publicación enunciado: 07/07/22
+ * Fecha publicación resolución: 11/07/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que determine si dos vectores son ortogonales.
+ * - Los dos array deben tener la misma longitud.
+ * - Cada vector se podría representar como un array. Ejemplo: [1, -2]
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+const areOrthogonal = (vectorA, vectorB) => !(vectorA[0] * vectorB[0] + vectorA[1] * vectorB[1]);
+
+module.exports = { areOrthogonal };
