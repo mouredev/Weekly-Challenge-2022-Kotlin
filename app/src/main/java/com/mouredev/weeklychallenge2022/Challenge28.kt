fb0c09d70d2bf26ee64ea78694ae104a73f4c8b1
Jorkoh <kohru95@gmail.com>
2022-07-18 20:49:05 +0200

    Solución Reto #28

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge28.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge28.kt
@@ -24,3 +24,75 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+object VendingMachine {
+
+    data class Product(val id: Int, val name: String, val price: Int)
+
+    private val products = listOf(
+        Product(1, "product 1", 235),
+        Product(2, "product 2", 50),
+        Product(3, "product 3", 470),
+        Product(4, "product 4", 115),
+    )
+
+    private val allowedCoins = listOf(200, 100, 50, 10, 5)
+
+    sealed class Result(val change: List<Int>) {
+        class Failure(val message: String, change: List<Int>) : Result(change)
+        class Success(val productName: String, change: List<Int>) : Result(change)
+    }
+
+    fun purchase(productId: Int, coins: List<Int>): Result {
+        val product = products.firstOrNull { product ->
+            product.id == productId
+        } ?: return Result.Failure("Invalid product id", coins)
+        if (coins.any { coin -> coin !in allowedCoins }) {
+            return Result.Failure("At least one of the coins isn't allowed", coins)
+        }
+        if (product.price > coins.sum()) {
+            return Result.Failure("Coins aren't enough to pay for the product", coins)
+        }
+
+        val change = mutableListOf<Int>()
+        var leftToReturn = coins.sum() - product.price
+        for (coin in allowedCoins) {
+            if (leftToReturn == 0) break
+
+            change += List(leftToReturn / coin) { coin }
+            leftToReturn %= coin
+        }
+
+        return if (leftToReturn != 0) {
+            // Won't ever happen with this product prices and allowed coins
+            Result.Failure("Couldn't form a valid change with the allowed coins", coins)
+        } else {
+            Result.Success(product.name, change)
+        }
+    }
+}
+
+fun main() {
+    val testCases = listOf(
+        2 to listOf(100, 20, 50),   // Invalid coin
+        3 to listOf(50, 50, 10, 5), // Insufficient amount
+        0 to listOf(200, 50, 200),  // Invalid id
+        1 to listOf(50, 50, 200),   // Inexact amount
+        4 to listOf(5, 5, 5, 100),  // Exact amount
+    )
+
+    testCases.forEach { (productId, coins) ->
+        println("Purchasing product with id: $productId using coins: ${coins.joinToString()}")
+        when (val result = VendingMachine.purchase(productId, coins)) {
+            is VendingMachine.Result.Failure -> {
+                println("Error purchasing: ${result.message}")
+                println("Returned change: ${result.change.joinToString()}")
+            }
+            is VendingMachine.Result.Success -> {
+                println("Purchased product: ${result.productName}")
+                println("Returned change: ${result.change.joinToString()}")
+            }
+        }
+        println()
+    }
+}
