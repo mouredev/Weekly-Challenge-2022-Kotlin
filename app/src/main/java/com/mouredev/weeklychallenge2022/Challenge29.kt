211ce6fb0336f63459a69adb47e6638aa39717d5
Miguel Ángel Delgado <migue@miguelandelgado.com>
2022-07-19 18:16:01 +0200

    Solución Reto #29

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge29.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge29.kt
@@ -19,3 +19,32 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main() {
+    println("${arraySort(listOf<Int>(2, 4, 7, 3, 6, 9, 5, 1, 0), "Asc")}")
+    println("${arraySort(listOf<Int>(2, 4, 7, 3, 6, 9, 5, 1, 0), "Desc")}")
+    println("${arraySort(listOf<Int>(9, 1, 8), "Asc")}")
+    println("${arraySort(listOf<Int>(9, 8, 1), "Desc")}")
+    println("${arraySort(listOf<Int>(3, 9, 8, 5, 1, 8), "Asc")}")
+    println("${arraySort(listOf<Int>(8), "Desc")}")
+}
+
+fun arraySort(items:List<Int>, order: String):List<Int>{
+    if (items.count() < 2){
+        return items
+    }
+
+    val pivot = items[items.count()/2]
+    val equal = items.filter { it == pivot }
+    val less = items.filter { it < pivot }
+    val greater = items.filter { it > pivot }
+
+    if (order == "Asc") {
+        return arraySort(less, order) + equal + arraySort(greater, order)
+    }
+    else {
+        return arraySort(greater, order) + equal + arraySort(less, order)
+    }
+}
+
+
