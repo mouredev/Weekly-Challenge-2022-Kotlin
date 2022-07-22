67fc28ce7b0c9d693d1178c3d4eff996a517e7d6
Miguel Angel Lopez <malopez@coviran.es>
2022-07-22 11:38:13 +0200

    Solucion Reto#29 kotlin mergesort

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge29.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge29.kt
@@ -19,3 +19,93 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+
+/**
+ * Funcion Principal
+ */
+fun main() {
+
+    val list = listOf(28, 11, 96, -5, 21, 18, 12, 22, 30, 97, -1, -40, -500)
+    println("Lista: ${list}")
+
+    println("Orden Ascendente: ${mergeSort(list, Order.ASC)}")
+    println("Orden Descendente: ${mergeSort(list, Order.DESC)}")
+}
+
+/**
+ * Enumerado que representa el orden de la lista.
+ */
+enum class Order {
+    ASC,
+    DESC
+}
+
+/**
+ * Función que ordena una lista de números Mediante el método de Merge Sort.
+ * @see https://es.wikipedia.org/wiki/Ordenamiento_por_mezcla
+ * @see https://www.youtube.com/watch?v=dENca26N6V4
+ * @param list Lista de números a ordenar.
+ * @param order Orden de la lista.
+ * @see Order
+ * @return Lista ordenada en orden ascendente o descendente.
+ */
+fun mergeSort(list: List<Int>, order: Order): List<Int> {
+
+    if (list.size == 1) {
+        return list
+    }
+
+    val mid = list.size / 2
+    val left = list.subList(0, mid)
+    val right = list.subList(mid, list.size)
+
+    return merge(mergeSort(left, order), mergeSort(right, order), order)
+
+}
+
+/**
+ * Función que combina dos listas de números en una sola lista ordenada
+ * @param order Orden de la lista Ascedente o descentente
+ * @param left Lista de números de la izquierda.
+ * @param right Lista de números a la derecha
+ * @return Lista de números ordenados.
+ */
+fun merge(left: List<Int>, right: List<Int>,order: Order) : List<Int> {
+    val result = mutableListOf<Int>()
+    var i = 0
+    var j = 0
+    while (i< left.size && j < right.size) {
+
+        if(order== Order.ASC){
+            if (left[i] < right[j]) {
+                result.add(left[i])
+                i++
+            } else {
+                result.add(right[j])
+                j++
+            }
+            }
+        else{
+
+            if (left[i] > right[j]) {
+                result.add(left[i])
+                i++
+            } else {
+                result.add(right[j])
+                j++
+            }
+
+        }
+    }
+    while (i < left.size) {
+        result.add(left[i])
+        i++
+    }
+    while (j < right.size) {
+        result.add(right[j])
+        j++
+    }
+
+    return result
+}
