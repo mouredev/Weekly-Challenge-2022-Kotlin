e8da8829bc898d1681d6aef28675438dd37bcafa
Anthony Lopez Vega <anthodev1195@gmail.com>
2022-07-26 17:49:29 -0500

    Solución Reto #30

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge30.ts
@@ -0,0 +1,41 @@
+const DEFAULT_QUANTITY = 4;
+
+function main(text: string): string {
+  let longitude = 0;
+  let transformedWord = "";
+
+  if (!text) return "";
+
+  const words = text.split(" ");
+  longitude = getMaxLongitude(words);
+
+  for (let word of words) {
+    while (word.length < longitude) word += " ";
+    transformedWord += "* " + word + " *\n";
+  }
+
+  transformedWord =
+    generateAsterisks(longitude) +
+    transformedWord +
+    generateAsterisks(longitude);
+
+  return transformedWord;
+}
+
+const getMaxLongitude = (words: string[]): number => {
+  let longitude = 0;
+  for (let word of words)
+    longitude = longitude > word.length ? longitude : word.length;
+  return longitude;
+};
+
+const generateAsterisks = (quantity: number): string => {
+  let asterisks = "";
+  for (let i = 0; i < quantity + DEFAULT_QUANTITY; i++) asterisks += "*";
+  return asterisks + "\n";
+};
+
+console.log(main("Hola"));
+console.log(main("¿Qué te parece el reto?"));
+console.log(main("Tres tristes tigres, tragaban trigo en un trigal, en tres tristes trastos, tragaban trigo tres tristes tigres"));
+console.log("");
