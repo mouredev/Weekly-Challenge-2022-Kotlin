0de6b25ca0d8d11998e7836d6c8771eeed7c11d3
Elio1992 <93145721+Elio1992@users.noreply.github.com>
2022-07-28 17:17:10 +0200

    Update Challenge30.kt
    Reto 30 en Java

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge30.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge30.kt
@@ -25,3 +25,71 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+package retosemanal;
+
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author elio
+ */
+public class RetoSemanal {
+
+    public static void main(String[] args) {
+
+        System.out.println(marco("Esto es una prueba."));
+        System.out.println("\n\n");
+        System.out.println(marco("Mi primer reto superado con exito."));
+    }
+
+    public static String marco(String texto) {
+
+        StringTokenizer palabras = new StringTokenizer(texto);
+        String palabra;
+        int numPalabras = palabras.countTokens();
+        int palabraMasLarga = longitudPalabras(texto);
+
+        String cadena = lineaAsteriscos(palabraMasLarga) + "\n";
+
+        for (int i = 0; i < numPalabras; i++) {
+            palabra = palabras.nextToken();
+            while (palabra.length() < palabraMasLarga) 
+                palabra = palabra + " ";
+            
+            cadena = cadena + "* " + palabra + " *" + "\n";
+        }
+        
+        cadena = cadena + lineaAsteriscos(palabraMasLarga);
+        return cadena;
+    }
+
+    public static int longitudPalabras(String texto) {
+
+        StringTokenizer palabras = new StringTokenizer(texto);
+        int palabraMasLarga = 0;
+        String palabra;
+        int numPalabras = palabras.countTokens();
+
+        for (int i = 0; i < numPalabras; i++) {
+            palabra = palabras.nextToken();
+            if (palabraMasLarga < palabraMasLarga(palabra)) 
+                    palabraMasLarga = palabraMasLarga(palabra);
+            
+        }
+        return palabraMasLarga;
+    }
+
+    public static int palabraMasLarga(String palabra) {
+        return palabra.length();
+    }
+
+    public static String lineaAsteriscos(int numeroAsteriscos) {
+        String lineaAsteriscos = new String();
+
+        for (int i = 0; i < numeroAsteriscos + 4; i++) 
+            lineaAsteriscos = lineaAsteriscos + "*";        
+        return lineaAsteriscos;
+    }
+
+}
