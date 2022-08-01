3d71d31062a44d4b9a5e7f0331821a161d1ce70d
jimenezbaquero <36899305+jimenezbaquero@users.noreply.github.com>
2022-08-01 22:03:17 +0200

    Resolucion del reto semanal 31 con PHP - 4 líneas

---
+++ app/src/main/Soluciones PHP/week31.php
@@ -0,0 +1,31 @@
+/*
+* Reto #31
+* AÑOS BISIESTOS
+* Fecha publicación enunciado: 01/08/22
+* Fecha publicación resolución: 08/08/22
+* Dificultad: FÁCIL
+*
+* Enunciado: Crea una función que imprima los 30 próximos años bisiestos siguientes a uno dado.
+* - Utiliza el menor número de líneas para resolver el ejercicio.
+*
+* Información adicional:
+* - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+* - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+* - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+* - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+*
+*/
+
+<?php
+
+pintar30bisiestos(2022);
+
+function pintar30bisiestos($año){
+    for($encontrados = 0; $encontrados < 30; $año++){
+        if (date('L', strtotime($año."-01-01"))) {
+            echo $año . ' ';
+            $encontrados++;
+        }
+    }
+}
+
