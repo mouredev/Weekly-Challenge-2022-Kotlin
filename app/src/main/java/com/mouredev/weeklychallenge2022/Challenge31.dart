2d6553a91b9ca0b6b98b6e3d260921100bf2eb66
Jjony-dev <developerjjm@gmail.com>
2022-08-03 15:51:04 -0300

    Solucion Reto#31 - Dart

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge31.dart
@@ -0,0 +1,29 @@
+/*
+ * Reto #31
+ * AÑOS BISIESTOS
+ * Fecha publicación enunciado: 01/08/22
+ * Fecha publicación resolución: 08/08/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea una función que imprima los 30 próximos años bisiestos siguientes a uno dado.
+ * - Utiliza el menor número de líneas para resolver el ejercicio.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+void main() {
+  leapYears(int y, int i) {
+    if (i <= 0) return;
+    y % 4 == 0 && (!(y % 100 == 0) || y % 400 == 0) ? print('$i\t$y') : i++;
+    i--;
+    leapYears(++y, i);
+  }
+
+  leapYears(2090, 30);
+  // leapYears(2022, 0);
+  // leapYears(2090, 1000);
+}
