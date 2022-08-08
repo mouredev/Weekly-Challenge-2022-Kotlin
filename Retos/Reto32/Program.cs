263aae3b16257d08037e4b79bf2afe0f9224d8e9
Juan Sanchez <gzki86@gmail.com>
2022-08-08 17:19:07 -0500

    Dando orden al proyecto unificando mis aportaciones en una la carpeta Retos

---
+++ Retos/Reto32/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> numberList = new List<int>(){
+    5, 8, 10, 4, 2, 45, 35, 11, 100, 25
+};
+
+int largest = 0;
+int secondLargest = 0;
+
+foreach (var number in numberList){
+    if(number > largest){
+        secondLargest = largest;
+        largest = number;
+    }else if(number > secondLargest){
+        secondLargest = number;
+    }
+    //Console.WriteLine(number);
+}
+
+Console.WriteLine("El segundo numero mayor es: {0}", secondLargest);
