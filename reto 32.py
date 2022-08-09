99b4a10c4a93739d32a3b2525dd88791064397c6
Israel Gómez Orviz <109038107+IsraGomOrv@users.noreply.github.com>
2022-08-09 23:44:09 +0200

    Modificación del reto 32#
    Modificación del envío del primer archivo enviado para la resolución del reto #32

--- reto 32.py
+++ reto 32.py
@@ -5,21 +5,26 @@
  # Dificultad: FÁCIL
  #
  # Enunciado: Dado un listado de números, encuentra el SEGUNDO más grande.
+import time  #  Importar la librería time.
 
-listado = []  # Declaración de la lista
+listado = []  # Declaración de la lista.
 
 while True:
     try:
-        numero = int(input("Introduce un número  \"0 \" para terminar..."))
+        numero = int(input("Introduce un número  \"0 \" para terminar..."))  # Introducción de números.
+        if numero == 0:
+            break
         listado.append(numero)
     except(ValueError):
-        break
+        print("Entrada erronea. ")
+        time.sleep(2)
+        continue
 
-for i in range(len(listado)):
+for i in range(len(listado)):  # Ordena la lista. 
     for j in range(len(listado)):
         if listado[i] > listado[j]:
             pasavalor = listado[i]
             listado[i] = listado[j]
             listado[j] = pasavalor
 
-print("El número buscado es ", listado[1])
+print("El número buscado es ", listado[1])  # Muesetra la lista.
