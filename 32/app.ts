9da15a7ad261d3ca5cb5c4e336f2d831a35f84c7
Fran Mella <franpola14@gmail.com>
2022-08-09 12:32:19 +0200

    Changes

--- 32/app.ts
+++ 32/app.ts
@@ -8,12 +8,15 @@
 // El segundo numero mas grande es: 19
 
 // Declaramos una variable con el array de numeros
-const numbers = [24, 16, 0, 123, 565, 34, 445, 12, 3];
+const numbers = [24, 16, 0, 123, 565, 34, 445, 445, 445, 12, 3];
+
+// Borramos los items duplicados y creamos un nuevo array sin los duplicados
+const uniqueNumbers = [...new Set(numbers)];
 
 // Declaramos una variable que guardarar el segundo numero mas grande.
 // Utilizamos la funcion .sort() para ordenar el array de numeros ordenador de mayor a menor.
 // Al ser un array seleccionamos la posición [1] que sería el segundo más grande.
-const secondMaxNumber = numbers.sort((a, b) => b - a)[1];
+const secondMaxNumber = uniqueNumbers.sort((a, b) => b - a)[1];
 // Resultado actual sin seleccionar la posicion [1]
 // [ 565, 445, 123, 34, 24, 16, 12, 3, 0 ]
 
