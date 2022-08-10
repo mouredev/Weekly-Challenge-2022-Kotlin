79b5d0c628eeb20674be81e8d84394b037c129a9
Manu <manuel.chavez@accenture.com>
2022-08-10 18:54:56 -0500

    Solucion reto 32 con C# .net 6

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenges/Challenge32/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var list = new List<int> {2,30,45,24,55,20,11};
+
+var secondNumber = list.OrderBy(c => c).Skip(1).FirstOrDefault();
+
+Console.WriteLine($"Second number is {secondNumber}");
