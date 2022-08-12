18d6a93f5699d27ebea71f861091592b146e3c49
Alberto Marqués <amarques74@gmail.com>
2022-08-12 15:11:18 +0200

    Update Challenge32.java
    Se eliminan condiciones rebundantes

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge32.java
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge32.java
@@ -3,16 +3,16 @@ import java.util.List;
 
 public class SegundoMayor {
     /**
-     * Implementación de menor complejidad (complejidad lineal O(n)) solo se recorre una vez la lista
+     * Implementación de menor complejidad (complejidad lineal) solo se recorre una vez la lista
      * y se dejan los 2 mayores encontrados. En caso de encontrar un mayor pasara el mayor actual
-     * detectado al segundo lugar. 
+     * detectado al segundo lugar.
      * <p>
-     * Si ordenaramos el array y lo recorrieramos hasta detectar un cambio tambíen lo encontrariamos
-     * pero la complejidad dejaría de ser lineal. El método de ordenación collections.sort complejjidad O(n log (n))
+     * Si ordernaramos el array y lo recorrieramos hasta detectar un cambio tambíen lo encontrariamos
+     * pero la complejidad dejaría de ser lineal.
      *
      */
     public static void main(String[] args) {
-        Double[] numbers = {1.5, 2.0, 11.0, 3.0, 5.0, 11.0, 9.0, 3.0, 3.0009, 2.03, 2.05};
+        Double[] numbers = {1.5, 2.0, 11.0, 3.0, 5.0, 11.0, 9.0, 3.0, 3.0009, 2.03, 2.05, 9.0000001};
         Double[] mayores = {-1.0, -1.0};
 
         List<Double> listNumbers = Arrays.asList(numbers);
@@ -24,10 +24,10 @@ public class SegundoMayor {
                     double segundoMayor = listMayores.get(1);
 
                     if(number!=primeroMayor && number!=segundoMayor){
-                        if (number != primeroMayor && number > primeroMayor) {
+                        if (number > primeroMayor) {
                             listMayores.set(0, number);
                             listMayores.set(1, primeroMayor);
-                        } else if (number != segundoMayor && number > segundoMayor) {
+                        } else if (number > segundoMayor) {
                             listMayores.set(1, number);
                         }
                     }
