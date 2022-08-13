3173f4368a2c5ea0b4416631d41e0be9dc7389af
Ertondo <gustavoarmitano@gmail.com>
2022-08-13 20:07:11 -0300

    Solucion Reto #2 - Fibonacci

---
+++ reto_2_fibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(num) {
+  let num1 = 0;
+  let num2 = 1;
+  let sum = 0;
+  console.log("0");
+  for (let i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num2 = num1;
+    num1 = sum;
+    console.log(sum);
+  }
+}
+
+fibonacci(50);
