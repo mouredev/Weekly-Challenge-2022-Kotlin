22d928fe7aa0770eb48a8aaaf4c94afa8cbee4a5
Brais Moure <braismoure@mouredev.com>
2022-08-15 21:39:13 +0200

    Resolución Reto #32 y enunciado Reto #33

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge33.kt
@@ -0,0 +1,23 @@
+package com.mouredev.weeklychallenge2022
+
+/*
+ * Reto #33
+ * CICLO SEXAGENARIO CHINO
+ * Fecha publicación enunciado: 15/08/22
+ * Fecha publicación resolución: 22/08/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un función, que dado un año, indique el elemento y animal correspondiente
+ * en el ciclo sexagenario del zodíaco chino.
+ * - Información: https://www.travelchinaguide.com/intro/astrology/60year-cycle.htm
+ * - El ciclo sexagenario se corresponde con la combinación de los elementos madera,
+ *   fuego, tierra, metal, agua y los animales rata, buey, tigre, conejo, dragón, serpiente,
+ *   caballo, oveja, mono, gallo, perro, cerdo (en este orden).
+ * - Cada elemento se repite dos años seguidos.
+ * - El último ciclo sexagenario comenzó en 1984 (Madera Rata).
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro Discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Tienes toda la información sobre los retos semanales en https://retosdeprogramacion.com/semanales2022.
+ *
+ */
