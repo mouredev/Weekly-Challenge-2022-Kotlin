ec05e7a7dcd12fa6b4b786fc8f226ff9aede311e
Darwing Rodilso Hernandez Castellanos <darwing01000100@gmail.com>
2022-08-25 11:16:56 -0600

    Solución Reto #34 kotlin" Numeros perdidos.

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge34.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge34.kt
@@ -18,4 +18,98 @@ package com.mouredev.weeklychallenge2022
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+fun main(){
+    var list= mutableListOf<Int>(2,3,4,56)
+    println(listNumbers(list));
+}
 
+
+private fun listNumbers(myArray: List<Int>){
+    try
+    {
+        if(!validateList(myArray)){
+            println("error: arreglo de datos no valido(desordenado)")
+            return
+        }
+        if(numbersRepeated(myArray)){
+            println("error: arreglo de datos no valido(repetidos)")
+            return
+        }
+
+        println("Numeros que faltan: "+listNum(myArray))
+    }catch (ex:TypeCastException){
+        print("Datos no validos")
+
+    }
+
+}
+
+/*
+    Validamos si el arreglo de entrada esta ordenado comparando con el mismo ordenado(usando la funcion sort),
+    en caso de estar ordenado retornamos un valor booleano */
+private fun validateList(myArray: List<Int>):Boolean{
+    var validate:Boolean=false
+    for (i in 0 until (myArray.size)){
+        var arraySort=myArray.sorted()
+        //comparamos si el array original es igual al ordenado.
+        if( myArray[i]==arraySort[i]){
+            validate = true
+
+        }else{
+            validate = false
+            break
+        }
+
+    }
+
+    return validate;
+}
+
+/*
+* validamos para que no hayan datos repetidos en el array
+* */
+private fun numbersRepeated(myArray: List<Int>):Boolean{
+    var validateRepeated=false
+    for (n in 1 until(myArray.size) ){
+        /*buscamos dos numeros que sean igual,
+         en todo el arreglo, y cuando lo encontrmos detenemos la busqueda*/
+        if((myArray[n])== (myArray[n-1])) {
+            validateRepeated = true
+            break
+        }
+
+    }
+    return validateRepeated
+}
+
+
+/* en esta funcion retornamos el nuevo arreglo con los datos faltantes*/
+private fun listNum(myArray: List<Int>): List<Int> {
+
+    val arrayNew= mutableListOf<Int>() //array para todos los numeros que faltan y los que ya existen
+
+
+    val numLost= mutableListOf<Int>()  //solo los numeros perdidos
+
+    arrayNew.add(myArray[0])
+    for (n in 1 until(myArray.size) ){
+
+        var countLost:Int=myArray[n]-myArray[n-1]
+
+        if(countLost>1){
+
+            for(i in 1 until (countLost)){
+                var num=myArray[n-1]+i
+                numLost.add(num);
+                arrayNew.add(num);
+
+            }
+
+        }
+
+        arrayNew.add(myArray[n])
+    }
+
+    return  numLost.toList(); //retorna solo los numeros que faltan
+
+}
