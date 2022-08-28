45697d36de8988ceb61b84af0c9f03ed3e0fd8ce
mariohgu <mario@handong.ac.kr>
2022-08-28 23:54:40 +0900

    Reto resuelto usando set, Python

---
+++ numero_perdido.py
@@ -0,0 +1,21 @@
+
+'''/*
+ * Enunciado: Dado un array de enteros ordenado y sin repetidos, 
+ * crea una función que calcule y retorne todos los que faltan entre
+ * el mayor y el menor.
+ * - Lanza un error si el array de entrada no es correcto.
+ */
+'''
+
+def missing(arra: list):
+    assert type(arra) is list, "No es array"
+    arra = list(set(arra))    
+    x = min(arra)
+    y = max(arra)    
+    new_arr =[n for n in range(x,y) if n not in arra]
+    print (new_arr)    
+
+def run():    
+    missing([5,15,8,3,10,9,15,3,2,10])
+
+if __name__ == '__main__':run()
