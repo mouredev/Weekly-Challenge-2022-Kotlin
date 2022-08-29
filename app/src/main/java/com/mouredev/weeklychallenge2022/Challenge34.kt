13f998aedcfb3d4a11c2bd4265c3151cd6ac8968
caldoDePoll0 <112378988+caldoDePoll0@users.noreply.github.com>
2022-08-29 10:03:10 -0500

    Update Challenge34.kt
    Solution

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge34.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge34.kt
@@ -19,3 +19,28 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    getRemainingNumbers(listOf(6, 5, 12, 8, 3, 9, 15))
+    getRemainingNumbers(listOf(2,"a",5,1, 8, 9))
+    getRemainingNumbers(listOf(2,true,5,1, 8, 9))
+    getRemainingNumbers(listOf(5, 3, -6, -2, 10, 8, 0))
+    getRemainingNumbers(listOf(1, 2, 2, 4, 15, 6, 10))
+}
+
+fun getRemainingNumbers(list: List<Any>) {
+
+    val containsNumbers = list.all { it is Int }
+    if (containsNumbers) {
+        list.map { it.toString().toInt() }.sorted().apply {
+            val remaining = generateSequence(first()) { it + 1 }
+                .filter { !contains(it) }
+                .takeWhile { it < last() }
+                .toList()
+
+            println("Remaining: $remaining")
+        }
+    } else {
+        println("List elements aren't numbers")
+    }
+
+}
