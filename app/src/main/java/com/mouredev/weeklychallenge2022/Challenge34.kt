5ae69da7d216d4781c958041b71c66fe0e4bd583
Felix Escandon <96452288+Fenixlix@users.noreply.github.com>
2022-08-29 13:14:59 -0500

    Challenge #34 solution

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge34.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge34.kt
@@ -19,3 +19,66 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    val example1 = arrayOf(1,3,6,8)
+    val example2 = arrayOf(10,1)
+    val example3 = arrayOf(5,7,2,1)
+    val example4 = arrayOf(5)
+
+    lookupForMissingOnes(example1)?.asList()?: println("Wrong input \n")
+    lookupForMissingOnes(example2)?.asList()?: println("Wrong input \n")
+    lookupForMissingOnes(example3)?.asList()?: println("Wrong input \n")
+    lookupForMissingOnes(example4)?.asList()?: println("Wrong input \n")
+}
+
+private fun lookupForMissingOnes(numberList: Array<Int>): Array<Int>? {
+
+    val missingOnes = mutableListOf<Int>()
+    var completeListItemCounter = 0
+
+    // First validation filter
+    if (numberList.size < 2 || numberList.first() == numberList.last())
+        return null
+
+    if (numberList.first() > numberList.last()) {
+        // Down count
+        var nMissing = numberList.first() - numberList.size
+        while (nMissing > 0) {
+            val first = numberList[completeListItemCounter++]
+            val second = numberList[completeListItemCounter]
+            if (first - second > 1 ) {
+                var x = 1
+                repeat(first-second-1){
+                    missingOnes.add(first-x++)
+                    nMissing--
+                }
+            } else if (first - second != 1){
+                // Second validation filter
+                return null
+            }
+        }
+    } else {
+        // Up count
+        var nMissing = numberList.last() - numberList.size
+        while (nMissing > 0) {
+            val first = numberList[completeListItemCounter++]
+            val second = numberList[completeListItemCounter]
+
+            if (first - second < -1) {
+                var x = 1
+                repeat(second-first-1) {
+                    missingOnes.add(first + x++)
+                    nMissing--
+                }
+            } else if (first - second != -1){
+                // Second validation filter
+                return null
+            }
+        }
+    }
+
+    if (missingOnes.isNotEmpty())
+        println("In the number array: ${numberList.asList()} \n The missing ones are: $missingOnes \n")
+
+    return missingOnes.toTypedArray()
+}
