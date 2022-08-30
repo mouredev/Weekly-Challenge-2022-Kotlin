994a87ff7ee300d59ecfcb596bc7950244520a66
Felix Escandon <96452288+Fenixlix@users.noreply.github.com>
2022-08-29 20:15:15 -0500

    Challenge #35 Solution

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge35.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge35.kt
@@ -24,3 +24,140 @@ package com.mouredev.weeklychallenge2022
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+fun main(){
+    val odish = Pokemon(name = "Odish", elementType = Pokemon.Element.Grass,
+        attackPower = 100, defencePower = 30, movePower = 50)
+
+    val lechonk = Pokemon(name = "Lechonk", elementType = Pokemon.Element.Fire,
+        attackPower = 50, defencePower = 80, movePower = 90)
+
+    attack(odish, lechonk)
+    attack(lechonk, odish)
+}
+
+class Pokemon(val name : String, val elementType : Element,
+               val attackPower : Int, val defencePower : Int, val movePower : Int){
+
+    sealed class Element(
+        val weak : List<Element>,
+        val effective : List<Element>,
+        val resist : List<Element>,
+        val immune : List<Element>? = null){
+        object Electric : Element(
+            weak = listOf(Ground),
+            effective = listOf(Water, Flying),
+            resist = listOf(Electric, Flying, Steel))
+
+        object Water : Element(
+            weak = listOf(Grass, Electric),
+            effective = listOf(Ground, Rock, Fire),
+            resist = listOf(Fire, Ice, Steel, Water))
+
+        object Fire : Element(
+            weak = listOf(Water, Ground, Rock),
+            effective = listOf(Grass, Steel, Ice, Bug),
+            resist = listOf(Steel, Fire, Grass, Ice, Bug))
+
+        object Grass : Element(
+            weak = listOf(Fire, Ice, Flying, Poison, Bug),
+            effective = listOf(Water, Ground, Rock),
+            resist = listOf(Water, Grass, Electric, Ground))
+
+        object Ground : Element(
+            weak = listOf(Water, Ice, Grass),
+            effective = listOf(Fire, Electric, Poison, Rock, Steel),
+            resist = listOf(Rock, Poison),
+            immune = listOf(Electric))
+
+        object Ice : Element(
+            weak = listOf(Fire, Fighting, Steel, Rock),
+            effective = listOf(Grass, Flying, Dragon, Ground),
+            resist = listOf(Ice))
+
+        object Steel : Element(
+            weak = listOf(Ground, Fire, Fighting),
+            effective = listOf(Ice, Rock),
+            resist = listOf(Psychic, Dragon, Steel, Bug,
+                Ice, Normal, Grass, Flying, Rock))
+
+        object Rock : Element(
+            weak = listOf(Ground, Water, Grass, Steel, Fighting),
+            effective = listOf(Ice, Fire, Bug, Flying),
+            resist = listOf())
+
+        object Poison : Element(
+            weak = listOf(Ground, Psychic),
+            effective = listOf(Grass),
+            resist = listOf(Poison, Bug, Fighting, Grass))
+
+        object Flying : Element(
+            weak = listOf(Rock, Ice, Electric),
+            effective = listOf(Grass, Fighting, Bug),
+            resist = listOf(Grass, Fighting, Bug),
+            immune = listOf(Ground))
+
+        object Dark : Element(
+            weak = listOf(Fighting, Bug),
+            effective = listOf(Psychic, Ghost),
+            resist = listOf(Psychic, Dark, Ghost))
+
+        object Normal : Element(
+            weak = listOf(Fighting),
+            effective = listOf(),
+            resist = listOf(),
+            immune = listOf(Ghost))
+
+        object Ghost : Element(
+            weak = listOf(Ghost, Dark),
+            effective = listOf(Psychic, Ghost),
+            resist = listOf(Poison, Bug),
+            immune = listOf(Normal, Fighting))
+
+        object Fighting : Element(
+            weak = listOf(Flying, Psychic),
+            effective = listOf(Ice, Rock, Steel, Normal, Dark),
+            resist = listOf(Dark, Rock, Bug))
+
+        object Dragon : Element(
+            weak = listOf(Dragon, Ice),
+            effective = listOf(Dragon),
+            resist = listOf(Fire, Water, Electric, Grass))
+
+        object Bug : Element(
+            weak = listOf(Rock, Fire, Flying),
+            effective = listOf(Psychic, Grass, Dark),
+            resist = listOf(Fighting, Grass, Ground))
+
+        object Psychic : Element(
+            weak = listOf(Bug, Dark, Ghost),
+            effective = listOf(Fighting, Poison),
+            resist = listOf(Psychic, Fighting))
+
+        fun getEffectiveMultiplier(attackElement : Element) : Float {
+            return if (immune != null && immune.contains(attackElement))
+                0.0f
+            else if (resist.contains(attackElement))
+                0.5f
+            else if (weak.contains(attackElement))
+                2.0f
+            else
+                1.0f
+        }
+    }
+}
+
+fun attack(pokemonA : Pokemon, pokemonD : Pokemon){
+
+    val damage = pokemonA.movePower*(pokemonA.attackPower/pokemonD.defencePower.toFloat())
+    val effectiveMultiplier = pokemonD.elementType.getEffectiveMultiplier(pokemonA.elementType)
+
+    println("\n ${pokemonA.name} attacks ${pokemonD.name}.")
+    println("It did ${damage*effectiveMultiplier} points of damage.")
+    println(when(effectiveMultiplier){
+        0.0f -> "${pokemonD.name} is immune."
+        0.5f -> "It was not effective"
+        2.0f -> "It was super effective"
+        else -> ""
+    })
+}
--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.kt
@@ -23,41 +23,124 @@ import kotlin.collections.HashMap
  */
 
 object abc {
-   val abecedario : Array<Char> = arrayOf(
-       'a',
-       'b',
-       'c',
-       'd',
-       'e',
-       'f',
-       'g',
-       'h',
-       'i',
-       'j',
-       'k',
-       'l',
-       'm',
-       'n',
-       'o',
-       'p',
-       'q',
-       'r',
-       's',
-       't',
-       'u',
-       'v',
-       'w',
-       'x',
-       'y',
-       'z',
-       'á',
-       'é',
-       'í',
-       'ó',
-       'u'
-   )
+    val abecedario : Array<Char> = arrayOf(
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+        'á',
+        'é',
+        'í',
+        'ó',
+        'u'
+    )
 }
 
-fun main() {
+fun main(){
 
+    val parrafoTexto:String =
+        "Este es un párrafo de ejemplo para comprobar el " +
+                "funcionamiento del código; Debe de evaluar SI o NO como si fueran sí o no indistintamente." +
+                "tomando en cuenta todos los signos de puntuación. (o todo lo que no sea una letra)"
+    val textoClasificado : Map<String, Int>  = clasificar(parrafoTexto)
+
+    imprimirListaDePalabras(textoClasificado)
+
+}
+
+// Clasifica las palabras dentro del bloque de texto y devuelve un objeto de mapa con cada palabra y sus ocurrencias.
+fun clasificar(parrafoTexto: String):Map<String, Int>{
+
+    val parrafoTextoSeparado : List<String> = parrafoTexto.split(" ")
+    val parrafoClasificado : MutableMap<String, Int> = HashMap<String,Int>()
+
+    for (palabra in parrafoTextoSeparado){
+        val palabraNormalizada = normalizar(palabra)
+        if (parrafoClasificado.containsKey(palabraNormalizada)){
+            parrafoClasificado[palabraNormalizada] = (parrafoClasificado[palabraNormalizada]!!+1)
+        } else {
+            parrafoClasificado[palabraNormalizada] = 1
+        }
+    }
+
+    return parrafoClasificado
+}
+
+// Remueve signos de puntuación y convierte a minúsculas
+fun normalizar(palabra: String):String{
+
+    // convierte a minúsculas
+    var palabraMod =  palabra.toLowerCase()
+
+    // obtiene el último caracter de la palabra
+    var caracter = palabraMod.get(palabra.length - 1)
+
+    // recorre el abecedario comparando en el último elemento
+    var seEncontroSigno = true
+    do {
+        for (letra in abc.abecedario){
+            if (letra.equals(caracter)){
+                seEncontroSigno = false
+                break
+            }
+        }
+        if (seEncontroSigno){
+            palabraMod = palabraMod.subSequence(0,palabraMod.length-1).toString()
+            caracter = palabraMod.get(palabraMod.length - 1)
+            //println("Se quito un signo en ${palabraMod}")  // Por si se quisiera ver los signos removidos
+        }
+    }while (seEncontroSigno && palabraMod.length>0)
+
+    caracter = palabraMod.get(0)
+    // recorre el abecedario comparando en el último elemento
+    seEncontroSigno = true
+    // recorre el abecedario comparando en el primer elemento
+    do {
+        for (letra in abc.abecedario){
+            if (letra.equals(caracter)){
+                seEncontroSigno = false
+                break
+            }
+        }
+        if (seEncontroSigno){
+            palabraMod = palabraMod.subSequence(1,palabraMod.length).toString()
+            caracter = palabraMod.get(0)
+            //println("Se quito un signo en ${palabraMod}")  // Por si se quisiera ver los signos removidos
+        }
+    }while (seEncontroSigno && palabraMod.length>0)
+
+    return palabraMod
+}
+
+// Imprime cada una de las palabras contenidas dentro del texto clasificado.
+fun imprimirListaDePalabras(textoClasificado: Map<String, Int>){
+    for(palabra in textoClasificado){
+        val unaOMas : String = if(palabra.value>1){
+            "veces"
+        } else
+            "ves"
+        println("La palabra: \"${palabra.key}\" se repite ${palabra.value} $unaOMas")
+    }
 }
