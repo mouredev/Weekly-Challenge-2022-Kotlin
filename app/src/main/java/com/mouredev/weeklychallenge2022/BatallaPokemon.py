fd75e585171af17053e9c82372d2a3123779f48a
Joel Exposito Ortiz <110563718+Joelexor@users.noreply.github.com>
2022-09-01 10:27:39 +0200

    Update BatallaPokemon.py

--- app/src/main/java/com/mouredev/weeklychallenge2022/BatallaPokemon.py
+++ app/src/main/java/com/mouredev/weeklychallenge2022/BatallaPokemon.py
@@ -18,39 +18,39 @@ def batallaPokemon(tipoAtaque, tipoDefensa, ataque, defensa):
     tipoDefensaCorrecto = tipos.__contains__(tipoDefensa)
 
     if tipoAtaqueCorrecto == True and tipoDefensaCorrecto == True:
-        if tipoAtaque == "Agua":
-            if tipoDefensa == "Agua" or tipoDefensa == "Planta":
-                efectividad = 0.5
-            elif tipoDefensa == "Fuego":
-                efectividad = 2
-            elif tipoDefensa == "Electrico":
-                efectividad = 1
-        elif tipoAtaque == "Fuego":
-            if tipoDefensa == "Agua" or tipoDefensa == "Fuego":
-                efectividad = 0.5
-            elif tipoDefensa == "Electrico":
-                efectividad = 1
-            elif tipoDefensa == "Planta":
-                efectividad = 2
-        elif tipoAtaque == "Planta":
-            if tipoDefensa == "Planta" or tipoDefensa == "Fuego":
-                efectividad = 0.5
-            elif tipoDefensa == "Electrico":
-                efectividad = 1
-            elif tipoDefensa == "Agua":
-                efectividad = 2
-        elif tipoAtaque == "Electrico":
-            if tipoDefensa == "Planta" or tipoDefensa == "Electrico":
-                efectividad = 0.5
-            elif tipoDefensa == "Fuego":
-                efectividad = 1
-            elif tipoDefensa == "Agua":
-                efectividad = 2
-
-        daño = 50 * (ataque/defensa) * efectividad
-
-        print("El daño del Pokemon de %s al Pokemon de %s será de %d puntos de salud."%(tipoAtaque,tipoDefensa,daño))
-
+        if ataque >= 1 and ataque <= 100 and defensa >= 1 and defensa <= 100:
+            if tipoAtaque == "Agua":
+                if tipoDefensa == "Agua" or tipoDefensa == "Planta":
+                    efectividad = 0.5
+                elif tipoDefensa == "Fuego":
+                    efectividad = 2
+                elif tipoDefensa == "Electrico":
+                    efectividad = 1
+            elif tipoAtaque == "Fuego":
+                if tipoDefensa == "Agua" or tipoDefensa == "Fuego":
+                    efectividad = 0.5
+                elif tipoDefensa == "Electrico":
+                    efectividad = 1
+                elif tipoDefensa == "Planta":
+                    efectividad = 2
+            elif tipoAtaque == "Planta":
+                if tipoDefensa == "Planta" or tipoDefensa == "Fuego":
+                    efectividad = 0.5
+                elif tipoDefensa == "Electrico":
+                    efectividad = 1
+                elif tipoDefensa == "Agua":
+                    efectividad = 2
+            elif tipoAtaque == "Electrico":
+                if tipoDefensa == "Planta" or tipoDefensa == "Electrico":
+                    efectividad = 0.5
+                elif tipoDefensa == "Fuego":
+                    efectividad = 1
+                elif tipoDefensa == "Agua":
+                    efectividad = 2
+            daño = 50 * (ataque/defensa) * efectividad
+            print("El daño del Pokemon de %s al Pokemon de %s será de %d puntos de salud."%(tipoAtaque,tipoDefensa,daño))
+        else:
+            print("El ataque y/o defensa no estan entre los valores 1 y 100.")
     elif tipoAtaqueCorrecto == False and tipoDefensaCorrecto == True:
         print("El tipo del Pokemon atacante es incorrecto.")
     elif tipoDefensaCorrecto == False and tipoAtaqueCorrecto == True:
@@ -61,6 +61,10 @@ def batallaPokemon(tipoAtaque, tipoDefensa, ataque, defensa):
 batallaPokemon("Tierra","Fuego",70,55) # Error Tipo de Ataque
 batallaPokemon("Agua","Dragon",50,65) # Error Tipo de Defensa
 batallaPokemon("Hielo","Hada",85,25) # Error de los dos Tipos
+batallaPokemon("Agua","Fuego",0,25) # Error valor ataque
+batallaPokemon("Agua","Fuego",85,101) # Error valor defensa
+batallaPokemon("Agua","Fuego",150,25) # Error valor ataque
+batallaPokemon("Agua","Fuego",85,-5) # Error valor defensa
 batallaPokemon("Agua","Fuego",35,65) # Efectividad x2
 batallaPokemon("Planta","Fuego",70,5) # Efectividad x0.5
 batallaPokemon("Electrico","Fuego",45,55) # Efectividad x1
