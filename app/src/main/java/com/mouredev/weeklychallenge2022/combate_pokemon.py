004485af4c09393c515ad5a99aed037f5072a830
Rafael Roldán <rafaroldan93.es@gmail.com>
2022-09-02 18:31:29 +0200

    Solución Reto #35 Python

--- app/src/main/java/com/mouredev/weeklychallenge2022/combate_pokemon.py
+++ app/src/main/java/com/mouredev/weeklychallenge2022/combate_pokemon.py
@@ -1,3 +1,15 @@
+"""
+Enunciado: Crea un programa que calcule el daño de un ataque durante una batalla Pokémon.
+- La fórmula será la siguiente: daño = 50 * (ataque / defensa) * efectividad
+- Efectividad: x2 (súper efectivo), x1 (neutral), x0.5 (no es muy efectivo)
+- Sólo hay 4 tipos de Pokémon: Agua, Fuego, Planta y Eléctrico (buscar su efectividad)
+- El programa recibe los siguientes parámetros:
+    - Tipo del Pokémon atacante.
+    - Tipo del Pokémon defensor.
+    - Ataque: Entre 1 y 100.
+    - Defensa: Entre 1 y 100.
+"""
+
 import random
 
 
