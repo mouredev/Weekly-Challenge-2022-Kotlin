72340bb2b385162bd53fcab58843ae0ee6f27d1d
José Gregorio Aguilar D'Vicente <jgglobal12@gmail.com>
2022-09-04 20:23:47 -0400

    mi solución al ejercicio!!
    Mi nombre es José Gregorio y aquí les presento mi solución al ejercicio propuesto... fue muy entretenido!!

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge35.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge35.kt
@@ -24,3 +24,96 @@ package com.mouredev.weeklychallenge2022
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+const efectividadPokemones = {
+	agua: {
+		agua: 0.5,
+		fuego: 0.5,
+		planta: 2,
+		electrico: 2,
+	},
+	fuego: {
+		agua: 2,
+		fuego: 0.5,
+		planta: 0.5,
+		electrico: 1,
+	},
+	planta: {
+		agua: 0.5,
+		fuego: 2,
+		planta: 0.5,
+		electrico: 0.5,
+	},
+	electrico: {
+		agua: 1,
+		fuego: 1,
+		planta: 1,
+		electrico: 0.5,
+	},
+};
+
+const regExp = /[a-z]/i;
+
+const atacante = 'fuego';
+const defensor = 'elEctrico';
+const ataque = 10;
+const defensa = 5;
+
+const tiposPokemon = ['agua', 'fuego', 'planta', 'electrico'];
+
+function calcularDañoPokemon(atacante, defensor, ataque, defensa) {
+	const pokemonAtacante = atacante.toLowerCase().trim();
+	const pokemonDefensor = defensor.toLowerCase().trim();
+
+	let mensajeError = '';
+
+	// validar que las especies pokemón se encuentren disponibles
+	let pokemonAtacanteValido = false;
+	for (let i = 0; i < tiposPokemon.length; i++) {
+		if (pokemonAtacante === tiposPokemon[i]) {
+			pokemonAtacanteValido = true;
+		}
+	}
+
+	let pokemonDefensorValido = false;
+	for (let i = 0; i < tiposPokemon.length; i++) {
+		if (pokemonDefensor === tiposPokemon[i]) {
+			pokemonDefensorValido = true;
+		}
+	}
+
+	// validar que los nombres de las especies pokemón estén correctamente escritos
+
+	if (pokemonAtacanteValido === false || pokemonDefensorValido === false) {
+		mensajeError =
+			'ERROR: El nombre de cada tipo de pokemón, solo puede contener letras sin acentos o tildes';
+	}
+
+	// validar que tanto el ataque como la defensa sean un número
+
+	if (typeof ataque !== typeof 0 || typeof defensa !== typeof 0) {
+		mensajeError =
+			'ERROR: Tanto el ataque como la defensa deben ser un número';
+	}
+
+	// validar que tanto el ataque como la defensa sean un número entre 1 y 100
+
+	if (ataque < 1 || ataque > 100 || defensa < 1 || defensa > 100) {
+		mensajeError =
+			'ERROR: Tanto el ataque como la defensa deben ser un número entre 1 y 100';
+	}
+
+	// validar que el mensaje de error esté vacío
+	if (!mensajeError) {
+		const daño =
+			50 *
+			(ataque / defensa) *
+			efectividadPokemones[`${pokemonAtacante}`][`${pokemonDefensor}`];
+
+		return `El daño producido es de ${daño}`;
+	} else {
+		return mensajeError;
+	}
+}
+
+console.log(calcularDañoPokemon(atacante, defensor, ataque, defensa));
