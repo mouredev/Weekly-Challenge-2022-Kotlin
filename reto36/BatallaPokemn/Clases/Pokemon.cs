52564d2b89afb4b3c7ef0463c3321ed70b6767d3
Pedro Salazar <salazar9_5@hotmail.com>
2022-09-06 11:29:05 -0600

    Reto36 en c#

---
+++ reto36/BatallaPokemn/Clases/Pokemon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatallaPokemn.Clases
+{
+    internal class Pokemon
+    {
+        public string Nombre;
+        public float Tipo;
+        public float Ataque;
+        public int Defensa;
+        public float vida;
+
+        public ResultadoAtaque Ataca(Pokemon Defensor)
+        {
+            ResultadoAtaque Resultado = new ResultadoAtaque();
+            //50 * (ataque / defensa) * efectividad
+            Resultado = TipoPokemon.Efectividad(this, Defensor);
+            float Efectividad = Resultado.Efectividad;
+            float Dano = 50 * (Ataque / Defensa) * Efectividad;
+            Resultado.Dano= Dano;
+            return Resultado;
+        }
+
+     
+    }
+}
---
+++ reto36/BatallaPokemn/Clases/TipoPokemon.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatallaPokemn.Clases
+{
+   
+     
+
+    internal class TipoPokemon
+    {
+
+        public enum Tipo
+        {
+            Fuego = 1,
+            Agua = 2,
+            Planta = 3,
+            Electrico = 4
+        }
+
+        public static ResultadoAtaque Efectividad(Pokemon Atacante,Pokemon Defensor)
+        {
+            ResultadoAtaque Result = new ResultadoAtaque();
+
+            //Agua, Fuego, Planta y Eléctrico
+            float Efectividad = 0;
+            string Mensaje = "";
+            switch(Atacante.Tipo)
+            {
+                case (int)Tipo.Fuego:
+                    switch (Defensor.Tipo)
+                    {
+                        case (int)Tipo.Fuego:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                        case (int)Tipo.Agua:
+                            Efectividad = .5F;
+                            Mensaje = "no es muy efectivo";
+                            break;
+                        case (int)Tipo.Planta:
+                            Efectividad = 2;
+                            Mensaje = "súper efectivo";
+                            break;
+                        case (int)Tipo.Electrico:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                    }
+                    break;
+                case (int)Tipo.Agua:
+                    switch (Defensor.Tipo)
+                    {
+                        case (int)Tipo.Fuego:
+                            Efectividad = 2;
+                            Mensaje = "súper efectivo";
+                            break;
+                        case (int)Tipo.Agua:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                        case (int)Tipo.Planta:
+                            Efectividad = .5F;
+                            Mensaje = "no es muy efectivo";
+                            break;
+                        case (int)Tipo.Electrico:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                    }
+                    break;
+                case (int)Tipo.Planta:
+                    switch (Defensor.Tipo)
+                    {
+                        case (int)Tipo.Fuego:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                        case (int)Tipo.Agua:
+                            Efectividad = 2;
+                            Mensaje = "súper efectivo";
+                            break;
+                        case (int)Tipo.Planta:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                        case (int)Tipo.Electrico:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                    }
+                    break;
+                case (int)Tipo.Electrico:
+                    switch (Defensor.Tipo)
+                    {
+                        case (int)Tipo.Fuego:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                        case (int)Tipo.Agua:
+                            Efectividad = 2;
+                            Mensaje = "súper efectivo";
+                            break;
+                        case (int)Tipo.Planta:
+                            Efectividad = .5F;
+                            Mensaje = "no es muy efectivo";
+                            break;
+                        case (int)Tipo.Electrico:
+                            Efectividad = 1;
+                            Mensaje = "neutral";
+                            break;
+                    }
+                    break;
+            }
+            Result.Efectividad = Efectividad;
+            Result.Mensaje = Mensaje;
+
+            return Result;
+        }
+    }
+}
