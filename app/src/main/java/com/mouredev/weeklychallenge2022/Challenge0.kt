74079ce5664291265172f6ac09cb484b08f832b4
David Molina Pulgarin <68005809+dvd23m@users.noreply.github.com>
2022-09-08 16:22:42 +0200

    Update Challenge0.kt
    Reto1_Python

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -19,19 +19,12 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
-fun main() {
-
-    for (index in 1..100) {
-        val divisibleByThree = index % 3 == 0
-        val divisibleByFive = index % 5 == 0
-        if (divisibleByThree && divisibleByFive) {
-            println("fizzbuzz")
-        } else if (divisibleByThree) {
-            println("fizz")
-        } else if (divisibleByFive) {
-            println("buzz")
-        } else {
-            println(index)
-        }
-    }
-}
+for i in range(1, 101):
+    if (i % 3 == 0) & (i % 5 == 0):
+        print("fizzbizz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("bizz")
+    else:
+        print(i)
