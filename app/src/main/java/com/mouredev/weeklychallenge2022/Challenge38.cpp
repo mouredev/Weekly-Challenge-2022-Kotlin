034519578c400f525f043954ebaaa194b9d18e61
Fabian Morales <fabian.morales@outlook.com>
2022-09-20 10:31:07 -0500

    Mejorar orden del código Automatizar entradas de números

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge38.cpp
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge38.cpp
@@ -19,25 +19,24 @@ double convert(int num, int from, int to) {
 
     double sum = 0;
     int i = 0;
-	while (num > 0) {
-		int temp = num % to;
-		num /= to;
-		if(temp >= from) {
-			return 0;
-		}
+    while (num > 0) {
+        int temp = num % to;
+        num /= to;
+        if(temp >= from) {
+            return 0;
+        }
 
-		sum += temp * pow(from, i);
+        sum += temp * pow(from, i);
         i++;
-	}
+    }
     
     return sum;
 }
 
 int main() {
-    int num;
-	cout<<"Ingrese el número en base 2: ";
-	cin>>num;
-    double result = convert(num, 2, 10);
-    
-	cout<<"El número"<<num<<"[2] equivale a "<<result<<"[10]"<<endl;
+    int nums[] = {10, 111, 1011, 110110, 100100, 1110111};
+    for (int i = 0; i < sizeof(nums) / sizeof(int); i++) {
+        double result = convert(nums[i], 2, 10);
+        cout<<"El número "<<nums[i]<<"[2] equivale a "<<result<<"[10]"<<endl;
+    }
 }
