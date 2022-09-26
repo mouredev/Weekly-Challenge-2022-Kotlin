54c4b9069cb5c1a8cffb462d7382892f52c84e90
AlexPereira <114361606+AlexKum1980@users.noreply.github.com>
2022-09-26 10:19:20 +0200

    v.1.0.0
    Rematado

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge38.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge38.kt
@@ -38,11 +38,50 @@ fun main() {
     println("-----------------------------")
     print(deBinarioDecimal("1050"))
     println(" O resultado sería null")
+    println("-----------------------------")
+
+    print(deBinarioDecimal(1100011))
+    println(" O resultado sería 99")
+    println("-----------------------------")
+    print(deBinarioDecimal(11000111000111000))
+    println(" O resultado sería 101944")
+    println("-----------------------------")
+    print(deBinarioDecimal(1050))
+    println(" O resultado sería null")
+    println("-----------------------------")
 
 }
+/*
+* Función que recibe un numero binario en String y lo devuelve el valor en decimal en Int.
+*/
 
 fun deBinarioDecimal(binario:String):Int? {
 
+    val dous = 2.0
+    var decimal= 0
+    var exp = 0
+    var esBinario = true
+
+    for (x in binario.lastIndex downTo 0) {
+        if (binario[x]== '0' || binario[x] == '1') {
+            val dixito:Int = binario[x].toString().toInt()
+            val paraSumar = dixito*dous.pow(exp++).toInt()
+            decimal += paraSumar
+        } else {
+            esBinario = false
+            break
+        }
+    }
+
+    if (esBinario)  return decimal else return null
+}
+
+/*
+* Función que recibe un numero binario en Long (los binarios tienen muchos digitos) y lo devuelve el decimal en Int.
+*/
+
+fun deBinarioDecimal(numBin:Long):Int? {
+    val binario:String = numBin.toString()
     val dous = 2.0
     var decimal= 0
     var exp = 0
