877244faa1d6b50abba6d79a9b4ecce368cc0a9e
davidgd72k <davidgd72k@gmail.com>
2022-09-29 19:55:47 +0200

    Solución Reto #0 CSharp

---
+++ app/src/main/cs/Challenge0.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzzer;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\t\t* * Ejercicio #0 - FIZZ BUZZ * *");
+
+        for (int number = 1; number <= 100; number++)
+        {
+            bool isMultipleOf3 = number % 3 == 0;
+            bool isMultipleOf5 = number % 5 == 0;
+
+            if (isMultipleOf3 && isMultipleOf5)
+            {
+                Console.Write("fizzbuzz");
+            }
+            else if (isMultipleOf3)
+            {
+                Console.Write("fizz");
+            }
+            else if (isMultipleOf5)
+            {
+                Console.Write("buzz");
+            }
+            else
+            {
+                Console.Write(number);
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
