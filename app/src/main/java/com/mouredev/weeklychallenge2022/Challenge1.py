0fb6945bfdc285c02aa7a54868a8848fab6de08b
Rolando Sabatino <rjsabatino@gmail.com>
2022-10-01 15:09:41 -0300

    Solución reto #39 Python

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.py
@@ -0,0 +1,37 @@
+#
+#Reto #1
+#¿ES UN ANAGRAMA?
+#Fecha publicación enunciado: 03/01/22
+#Fecha publicación resolución: 10/01/22
+#Dificultad: MEDIA
+#
+#Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+#Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+#NO hace falta comprobar que ambas palabras existan.
+#Dos palabras exactamente iguales no son anagrama.
+#
+#Información adicional:
+#- Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+#- Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+#- Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+#- Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+#
+#
+
+def esAnagrama(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+
+    if(str1 == str2):
+        return False
+
+    if(len(str1) != len(str2)):
+        return False
+
+    for i in range(1, len(str1)):
+        if(str1[i] not in str2):
+            return False
+
+    return True
+
+print(esAnagrama('pelo','OlEp'))
