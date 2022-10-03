023da81440f74fb87f70235b8224ae342e922a70
AlbertoVf <avf.git-repos@protonmail.com>
2022-10-03 11:53:00 +0200

    Solucion reto#39

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge39.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge39.kt
@@ -23,5 +23,37 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    var array = arrayOf<Int>(1,5,9,0,6,2)
+    ordenar(array, 0, array.size - 1)
 
+    for (i in array) print("${i} ")
+}
 
+fun ordenar(array: Array<Int>, p: Int, r: Int) {
+    if (p < r) {
+        var q: Int = particionar(array, p, r)
+        ordenar(array, p, q - 1)
+        ordenar(array, q + 1, r)
+    }
+}
+
+fun particionar(array: Array<Int>, p: Int, r: Int): Int {
+
+    fun pivotar(array: Array<Int>, i: Int, j: Int) {
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+
+    var x = array[r]
+    var i = p - 1
+    for (j in p until r) {
+        if (array[j] <= x) {
+            i++
+            pivotar(array, i, j)
+        }
+    }
+    pivotar(array, i + 1, r)
+    return i + 1
+}
