12fc980a644a1f9717ca86076b8b9be0cdc1f0d4
Miguel Angel Lopez <malopez@coviran.es>
2022-10-04 12:50:35 +0200

    Solucion reto #39 Kotlin

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge39.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge39.kt
@@ -24,4 +24,78 @@ package com.mouredev.weeklychallenge2022
  */
 
 
+/**
+ *  Funcion Principal
+ *
+ */
+fun main(){
+
+    val array = arrayOf(5, 3, 8, 4, 2, 7, 1, 10, 6, 9)
+
+    println("Array original: ${array.contentToString()}")
+    println("Array ordenado: ${array.quickSort().contentToString()}")
+
+}
+
+/**
+ * Funcion que ordena un array de enteros mediante el algoritmo Quick Sort
+ * @return Array<Int> ordenado
+ * @param array Array<Int> a ordenar
+ * @param left Int indice izquierdo del array
+ * @param right Int indice derecho del array
+ */
+fun quickSort(array: Array<Int>,left:Int,right:Int): Array<Int> {
+
+    if(left < right) {
+        val pivot = partition(array, left, right)
+        quickSort(array, left, pivot - 1)
+        quickSort(array, pivot + 1, right)
+    }
+    return array
+}
+
+/**
+ * Funcion que particiona un array de enteros , usando la metodologia divide y venceras
+ * @return Int indice del pivote
+ * @param array Array<Int> a partir
+ * @param left Int indice izquierdo del array
+ * @param right Int indice derecho del array
+ * @param pivot Int indice del pivote
+ */
+
+fun partition(array: Array<Int>,left:Int,right:Int): Int {
+    val pivot = array[right]
+
+    var i = left
+    for(j in left until right) {
+        if(array[j] <= pivot) {
+            swap(array,i,j)
+            i++
+        }
+    }
+    swap(array,i,right)
+    return i
+
+}
+
+/**
+ * Funcion que intercambia dos elementos de un array
+ * @param array Array<Int> que contiene los elementos a intercambiar
+ * @param i Int indice del elemento que se intercambiará con el elemento de indice j
+ * @param j Int indice del elemento que se intercambiará con el elemento de indice i
+ */
+fun swap(array: Array<Int>,i:Int,j:Int) {
+    val temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+}
+
+
+/**
+ * Funcion de extension de Array que devuelve el array ordenado mediante el algoritmo Quick Sort
+ */
+fun Array<Int>.quickSort():Array<Int>{
+    return quickSort(this, 0, this.size - 1)
+}
+
 
