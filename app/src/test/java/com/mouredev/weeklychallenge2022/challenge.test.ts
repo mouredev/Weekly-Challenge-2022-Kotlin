13d0c8ec526caaec2ac58d9847cad8d77bd2cd69
Miguel Angel Lopez <malopez@coviran.es>
2022-10-04 11:06:55 +0200

    Update Test Typescript

--- app/src/test/java/com/mouredev/weeklychallenge2022/challenge.test.ts
+++ app/src/test/java/com/mouredev/weeklychallenge2022/challenge.test.ts
@@ -5,15 +5,16 @@ describe('Test Challenge Moure dev', () => {
     describe('Challenge #39', () => {
         it('should return order list :  ', () => {
             const list = [28, 11, 96, -5, 21, 18, 12, 22, 30, 97, -1, -40, -500];
-            var respuest = "2" //sort(list);
+            var respuest = sort(list);
             const list_ordered = [-500,-40,-5,-1,11,12,18,21, 22 , 28 , 30 , 96 , 97]
-            expect(respuest).to.equal("");
+            console.log(list_ordered)
+            expect(respuest).deep.equal(list_ordered)
         })
         it('should return order list :  ', () => {
              const list = [1,5,6,2,4]
              var respuest = sort(list);
              const list_ordered = [ 1,2,4,5,6];
-             expect(respuest).to.equal(list_ordered);
+             expect(respuest).deep.equal(list_ordered)
          })
  })}
 )
