1c1c00fa8ce6e62a534a4635942cff6e010494b4
Miguel Ángel Delgado <migue@miguelandelgado.com>
2022-10-08 17:24:22 +0200

    Solución Reto #40 - Kotlin

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge40.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge40.kt
@@ -20,5 +20,30 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    print("Ingrese el tamaño del lado: ")
+    val side = readln().toInt()
+    PascalTriangle(side)
+}
 
+private fun PascalTriangle(rows: Int){
+    var coef = 1
+    for (i in 0..rows - 1) {
+
+        for (space in 1..rows - i - 1) {
+            print("  ")
+        }
+
+        for (j in 0..i) {
+            if (j == 0 || i == 0)
+                coef = 1
+            else
+                coef = coef * (i - j + 1) / j
+
+            System.out.printf("%4d", coef)
+        }
+
+        println()
+    }
+}
 
