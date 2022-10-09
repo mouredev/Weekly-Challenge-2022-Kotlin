b87d8e9a27441aaa56a9e4f828040eb2380dc916
David Molina Pulgarin <68005809+dvd23m@users.noreply.github.com>
2022-10-09 07:33:02 +0200

    reto_40 python

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge40.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge40.kt
@@ -20,5 +20,15 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+from math import factorial
 
+def combinacion(n, r):
+    return factorial(n)//(factorial(r)*factorial(n-r))
 
+num = int(input("Introduce un número: "))
+
+for n in range(num):
+    row = []    
+    for r in range(n+1):
+        row.append(combinacion(n, r))
+    print(' '*(num-n) + " ".join(str(e) for e in row))
