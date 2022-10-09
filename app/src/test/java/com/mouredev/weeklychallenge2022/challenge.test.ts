1a03b682c7f1c4d1c6dc099d10d6b7ee0f384d12
Miguel Angel Lopez <malopez@coviran.es>
2022-10-09 23:33:01 +0200

    Solucion reto #40 Typescript

--- app/src/test/java/com/mouredev/weeklychallenge2022/challenge.test.ts
+++ app/src/test/java/com/mouredev/weeklychallenge2022/challenge.test.ts
@@ -1,15 +1,17 @@
 import { expect } from 'chai';
-import { calculatePokeDamage,PokemonType } from '../../../../../main/java/com/mouredev/weeklychallenge2022/Challenge35';
+import {pascalTriangle  } from "../../../../../main/java/com/mouredev/weeklychallenge2022/Challenge40";
 
 describe('Test Challenge Moure dev', () => {
-    describe('Challenge #35', () => {
-        it('should return 100 : Water(50) vs Fire(50) ', () => {
-            var respuest =  calculatePokeDamage(PokemonType.WATER,PokemonType.FIRE,50,50);
-            expect(respuest).to.equal(100)
+    describe('Challenge #40', () => {
+        it("should return Pascal Triangle (3) : ", () => {
+          let respuest = pascalTriangle(3);
+          let expected = [1, 1, 1, 1, 2, 1];
+          expect(respuest).deep.equal(expected);
         }),
-        it('should return 12.5 : Grass(20) vs Electric(80) ', () => {
-            var respuest =  calculatePokeDamage(PokemonType.GRASS,PokemonType.ELECTRIC,20,80);
-            expect(respuest).to.equal(12.5)
-        })
+        it("should return Pascal Triangle (7) : ", () => {
+          let respuest = pascalTriangle(7);
+          let expected = [1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1, 1, 5, 10, 10, 5, 1, 1, 6, 15, 20, 15, 6, 1];
+          expect(respuest).deep.equal(expected);
+          });
  })}
 )
