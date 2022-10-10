a02589ed0c620b4f17b75b36d6b7909e837350b2
PabloRoger <parovi97@gmail.com>
2022-10-11 00:59:36 +0200

    Solución Reto #1 JavaScript

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.js
@@ -0,0 +1,38 @@
+// package com.mouredev.weeklychallenge2022
+
+/*
+ * Reto #1
+ * ¿ES UN ANAGRAMA?
+ * Fecha publicación enunciado: 03/01/22
+ * Fecha publicación resolución: 10/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+function anagrama(p1, p2){
+    if (p1.toLowerCase()==p2.toLowerCase()){
+        return false;
+    }
+
+    let palabra1 = p1.toLowerCase().split("").sort();
+    let palabra2 = p2.toLowerCase().split("").sort();
+
+    for(let i=0;i<palabra1.length;i++){
+        if(palabra1[i]!==palabra2[i]){
+        return false;
+        }
+    }
+        return true;
+    }
+
+    anagrama("amor","roma");
