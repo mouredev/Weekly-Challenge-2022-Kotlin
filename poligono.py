571e34d0513a386c7cd8d0e43de3b904a2268de8
leo-cerdan <leocerdan@gmail.com>
2022-10-11 11:19:33 +0200

    Solución Reto #4 Python

---
+++ poligono.py
@@ -0,0 +1,31 @@
+'''
+ * Reto #4
+ * ÁREA DE UN POLÍGONO
+ * Fecha publicación enunciado: 24/01/22
+ * Fecha publicación resolución: 31/01/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea UNA ÚNICA FUNCIÓN (importante que sólo sea una) que sea capaz de calcular y retornar el área de un polígono.
+ * - La función recibirá por parámetro sólo UN polígono a la vez.
+ * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ * - Imprime el cálculo del área de un polígono de cada tipo.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+'''
+
+def area_poligono (poligono,base,altura=0):
+    if poligono == "cuadrado":
+        return base ** 2
+    elif poligono == "triangulo":
+        return base * altura / 2
+    elif poligono == "rectangulo":
+        return base * altura
+
+print(area_poligono("cuadrado", 5))
+print(area_poligono("triangulo", 2.5, 3.5))
+print(area_poligono("rectangulo", 7, 4.5))
