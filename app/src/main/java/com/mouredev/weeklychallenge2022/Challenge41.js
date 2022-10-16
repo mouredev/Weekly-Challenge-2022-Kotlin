8137d2d4fd0a00ded5eab373c2797dfc41a5a334
Jorge Aburto <jaburto@maimag.cl>
2022-10-15 23:05:06 -0300

    “Solución Reto #41 Javascript"

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge41.js
@@ -0,0 +1,36 @@
+
+/*
+ * Reto #41
+ * LA LEY DE OHM
+ * Fecha publicación enunciado: 10/10/22
+ * Fecha publicación resolución: 17/10/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea una función que calcule el valor del parámetro perdido correspondiente a la ley de Ohm.
+ * - Enviaremos a la función 2 de los 3 parámetros (V, R, I), y retornará el valor del tercero (redondeado a 2 decimales).
+ * - Si los parámetros son incorrectos o insuficientes, la función retornará la cadena de texto "Invalid values".
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro Discord (https://mouredev.com/discord) "🔁reto-semanal"
+ *   para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Tienes toda la información sobre los retos semanales en
+ *   https://retosdeprogramacion.com/semanales2022.
+ *
+ */
+
+
+
+function ohm({V, R, I}: {V?: number, R?: number, I?: number}) {
+  const paramsCounter = [V, R, I].filter((x) => x ).length;
+  if (paramsCounter < 2 || R === 0 || I === 0) {
+    return "Invalid values"
+  }
+  
+  return I && R ? Math.round((I * R) * 100) / 100 :
+				 V && R	? Math.round((V / R) * 100) / 100 :
+  			 V && I ? Math.round((V / I) * 100) / 100	:
+  			 NaN
+}
+
+
+ohm({V: 10, R: 2})
