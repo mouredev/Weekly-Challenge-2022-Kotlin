0f23c7bb43a01fecc8cd40e80e22748cf5ab0a55
Ramón G. Gisbert <gisbert.rg@gmail.com>
2022-10-20 21:27:54 +0200

    Solución Reto #3 TypeScript

---
+++ challenges/Reto-3/TypeScript/tests/unit/reto3.test.js
@@ -0,0 +1,35 @@
+const {isPrimeNumber} = require('../../reto3');
+
+describe('Test "isPrimeNumber" function', () => {
+  test('1 is not prime', () => {
+    expect(isPrimeNumber(1)).toBeFalsy();
+  });
+
+  test('2 is prime', () => {
+    expect(isPrimeNumber(2)).toBeTruthy();
+  });
+
+  test('7 is prime', () => {
+    expect(isPrimeNumber(7)).toBeTruthy();
+  });
+
+  test('11 is prime', () => {
+    expect(isPrimeNumber(2)).toBeTruthy();
+  });
+
+  test('54 is not prime', () => {
+    expect(isPrimeNumber(54)).toBeFalsy();
+  });
+
+  test('71 is prime', () => {
+    expect(isPrimeNumber(71)).toBeTruthy();
+  });
+
+  test('97 is prime', () => {
+    expect(isPrimeNumber(97)).toBeTruthy();
+  });
+
+  test('98 is not prime', () => {
+    expect(isPrimeNumber(98)).toBeFalsy();
+  });
+});
