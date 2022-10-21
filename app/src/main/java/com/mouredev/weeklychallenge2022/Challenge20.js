7638819b17e700a146c28b98fa553484cf6b3dbe
Pablo Roger <parovi97@gmail.com>
2022-10-21 13:28:36 +0200

    Solución #20 en JavaScript

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge20.js
@@ -0,0 +1,35 @@
+/*
+ * Reto #20
+ * PARANDO EL TIEMPO
+ * Fecha publicación enunciado: 16/05/22
+ * Fecha publicación resolución: 23/05/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea una función que sume 2 números y retorne su resultado pasados unos segundos.
+ * - Recibirá por parámetros los 2 números a sumar y los segundos que debe tardar en finalizar su ejecución.
+ * - Si el lenguaje lo soporta, deberá retornar el resultado de forma asíncrona, es decir, sin detener la ejecución del programa principal. Se podría ejecutar varias veces al mismo tiempo.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+function suma(int1, int2, segundos){
+    return new Promise(resuelve => {
+        setTimeout(() => {
+            resuelve(int1+int2);
+        }, segundos);
+    });
+}
+
+async function asyncSuma() {
+    console.log("Sumando los números...");
+    const resultado = await suma(30,40,2000);
+    console.log(resultado);
+    return resultado;
+}
+
+asyncSuma();
