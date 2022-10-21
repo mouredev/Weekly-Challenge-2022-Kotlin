c518653505633a6fee1e049b802063fb35cbcba7
emilianod98 <emidaniele98@gmail.com>
2022-10-21 11:28:59 -0300

    SolucionRetoSemanaPython0
    Solución del reto semanal 0 en Python

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.py
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.py
@@ -1,4 +1,4 @@
-package com.mouredev.weeklychallenge2022
+'''package com.mouredev.weeklychallenge2022
 
 /*
  * Reto #0
@@ -35,3 +35,14 @@ fun main() {
         }
     }
 }
+'''
+
+for num in range(1,101):
+    if (num % 3 == 0) and (num % 5 == 0):
+        print ('fizzbuzz')
+    elif num % 3 == 0:
+        print ('fizz')
+    elif num % 5 == 0:
+        print('buzz')
+    else:
+        print(num)
