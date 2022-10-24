9b5ae58f0849f0a8143a2d96f2d521474d8d1f29
Carlos Orejuela <50788797+IEdorasI@users.noreply.github.com>
2022-10-24 10:25:18 -0500

    Solución Reto #42: CONVERSOR DE TEMPERATURA
    Usando JavaScript

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge42.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge42.kt
@@ -21,3 +21,26 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+//Usando Javascript
+
+let valor;
+let resultado;  
+
+
+function convertir (){
+    valor = prompt('Ingresa un valor en °F o °C: ');
+    const faren = /\d\°F$/gi
+    const cent = /\d\°C$/gi
+    const comprobacion = faren.test(valor);
+    const comprobacion2 = cent.test(valor);
+    if (comprobacion === true ){
+        resultado = (parseInt(valor) - 32) / 1.8
+        alert(resultado);
+    } else if (comprobacion2 === true){
+        resultado = parseInt(valor) * 1.8 + 32;
+        alert(resultado);
+    }else {
+        alert ('Ingresa un valor aceptado')
+    }
+}
+
