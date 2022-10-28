186fc87687e4bf124d49a39c5ea4d6ef08b4e0fd
Gonzalo Piorno <gonzapior@gmail.com>
2022-10-27 21:33:08 -0300

    Solucion Challenge2 de mouredev

---
+++ retos-semanales/reto-1/Challenge2.js
@@ -0,0 +1,45 @@
+//package com.mouredev.weeklychallenge2022
+
+//import java.math.BigInteger
+
+/*
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ * Autor: Gonzalo Piorno
+ * Lenguaje: Javascript
+ */
+
+var numbers = [0];
+console.log(numbers[0])
+
+for (let i=0;i<50;i++) {
+
+    if (numbers.length==1) {
+        numbers.push(1);
+        console.log(numbers[1]);
+    }
+    else {
+        let number1 = numbers[numbers.length-1];
+        let number2 = numbers[numbers.length-2];
+
+        let resta = number1 + number2;
+
+        numbers.push(resta);
+        console.log(numbers[numbers.length-1]);
+    }
+    
+}
