671fabe45b45e7246278ab6135f9c7bd3a9984fe
aavv8931 <arturo_veintimilla@hotmail.com>
2022-10-29 23:17:27 -0500

    Resolucion Challenge0 JS

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.js
@@ -0,0 +1,15 @@
+function program(){
+    let i = 1
+      for(i; i <= 100; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            console.log("fizzbuzz")
+        }else if (i % 3 == 0) {
+              console.log("fizz")
+          }else if (i % 5 == 0){
+              console.log("buzz")
+          }else{
+          console.log(i)}
+  }
+  }
+  
+  program()
