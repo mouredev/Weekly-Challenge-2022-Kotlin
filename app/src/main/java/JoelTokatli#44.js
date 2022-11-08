e4b7b3de960e873fbf14dd84c8ec45fff87dce57
JoelTokatli <jamescarter6667@gmail.com>
2022-11-08 18:29:10 -0400

    añadiendo enunciado

--- app/src/main/java/JoelTokatli#44.js
+++ app/src/main/java/JoelTokatli#44.js
@@ -1,3 +1,13 @@
+/*
+ * Enunciado: Crea una función que retorne el número total de bumeranes de
+ * un array de números enteros e imprima cada uno de ellos.
+ * - Un bumerán (búmeran, boomerang) es una secuencia formada por 3 números
+ *   seguidos, en el que el primero y el último son iguales, y el segundo
+ *   es diferente. Por ejemplo [2, 1, 2].
+ * - En el array [2, 1, 2, 3, 3, 4, 2, 4] hay 2 bumeranes ([2, 1, 2]
+ *   y [4, 2, 4]).
+ */
+
 const array1 = [1, 0, 1, 0, 3, 5, 7, 4, 7, 4];
 
 const array2 = [5, 7, 5, 7, 5, 7, 5, 7, 5, 7, 5];
