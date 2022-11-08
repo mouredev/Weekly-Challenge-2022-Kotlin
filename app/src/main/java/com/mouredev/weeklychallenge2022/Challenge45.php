b40e67745db51ecc9b0ab396c88876a184599c65
Jon González Garrido <jon.gonzalez@atrapalo.com>
2022-11-08 08:21:05 +0100

    Challenge 45

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge45.php
@@ -0,0 +1,192 @@
+<?php
+/*
+ * Enunciado: Dado un array de números enteros positivos, donde cada uno
+ * representa unidades de bloques apilados, debemos calcular cuantas unidades
+ * de agua quedarán atrapadas entre ellos.
+ *
+ * - Ejemplo: Dado el array [4, 0, 3, 6, 1, 3].
+ *
+ *         ⏹
+ *         ⏹
+ *   ⏹💧💧⏹
+ *   ⏹💧⏹⏹💧⏹💧⏹
+ *   ⏹💧⏹⏹💧⏹⏹⏹⏹
+ *   ⏹💧⏹⏹⏹⏹⏹⏹⏹⏹
+ *
+ *   Representando bloque con ⏹︎ y agua con 💧, quedarán atrapadas 7 unidades
+ *   de agua. Suponemos que existe un suelo impermeable en la parte inferior
+ *   que retiene el agua.
+ */
+
+/** SOLUTION TEST */
+
+/*
+ *
+ */
+assertEquals(0, getNumberOfWater([]));
+
+
+/*
+ *
+ *
+ *   ⏹
+ *   ⏹
+ *   ⏹
+ *   ⏹
+ */
+assertEquals(0, getNumberOfWater([4]));
+
+
+
+/*
+ *     ⏹
+ *     ⏹
+ *   ⏹⏹
+ *   ⏹⏹
+ *   ⏹⏹
+ *   ⏹⏹
+ */
+assertEquals(0, getNumberOfWater([4, 6]));
+
+/*
+ *
+ *       ⏹
+ *     ⏹⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ */
+assertEquals(0, getNumberOfWater([3, 4, 5]));
+
+
+/*
+ *
+ *   ⏹
+ *   ⏹⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ */
+assertEquals(0, getNumberOfWater([5, 4, 3]));
+
+/*
+ *
+ *   ⏹💧⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ *   ⏹⏹⏹
+ */
+assertEquals(1, getNumberOfWater([5, 4, 5]));
+
+/*
+ *
+ *         ⏹
+ *         ⏹
+ *   ⏹💧💧⏹
+ *   ⏹💧⏹⏹💧⏹
+ *   ⏹💧⏹⏹💧⏹
+ *   ⏹💧⏹⏹⏹⏹
+ */
+assertEquals(7, getNumberOfWater([4, 0, 3, 6, 1, 3]));
+
+/*
+ *
+ *         ⏹
+ *         ⏹
+ *   ⏹💧💧⏹
+ *   ⏹💧⏹⏹💧⏹💧⏹
+ *   ⏹💧⏹⏹💧⏹⏹⏹⏹
+ *   ⏹💧⏹⏹⏹⏹⏹⏹⏹⏹
+ */
+assertEquals(8, getNumberOfWater([4, 0, 3, 6, 1, 3, 2, 3, 3, 1]));
+
+
+function assertEquals($expectedValue, $actualValue): void
+{
+    if ($expectedValue !== $actualValue) {
+        echo "$actualValue should be $expectedValue\n";
+    } else {
+        echo "OK\n";
+    }
+}
+
+/**
+ * This solution is not the most optimal solution but yes the one that is easier to maintain and understand. We
+ * could improve it if the number of blocks will be big, otherwise is preferred to have understandable code.
+ *
+ * @param array $blocksCountByRow
+ * @return int|mixed
+ */
+function getNumberOfWater(array $blocksCountByRow): int
+{
+
+    if (count($blocksCountByRow) < 3) {
+        return 0;
+    }
+
+    $watter = 0;
+
+    for ($row = 1; $row < count($blocksCountByRow) - 1; $row++) {
+        $currentRowBlocks = $blocksCountByRow[$row];
+
+        //Find the highest number of blocks in previous rows
+        $highestNumberOfBlocksInPreviousRows = findTheHighestBlockNumberBeingIncremental(
+            array_reverse(
+                array_slice(
+                    $blocksCountByRow,
+                    0,
+                    $row)
+            )
+        );
+
+        //Find the highest number of blocks in next rows
+        $highestNumberOfBlocksInNextRows = findTheHighestBlockNumberBeingIncremental(
+            array_slice(
+                $blocksCountByRow,
+                $row
+            )
+        );
+
+        if ($highestNumberOfBlocksInPreviousRows === null || $highestNumberOfBlocksInNextRows === null) {
+            throw new LogicException(
+                sprintf(
+                "This can't happen because we iterate the blocks from the second one to the penultimate. \n
+                Values: %s, %s",
+                    $highestNumberOfBlocksInPreviousRows,
+                    $highestNumberOfBlocksInNextRows
+                )
+            );
+        }
+
+        $lowerCountOfBLocks = min($highestNumberOfBlocksInPreviousRows, $highestNumberOfBlocksInNextRows);
+
+        if ($lowerCountOfBLocks >= $currentRowBlocks) {
+            $watter += $lowerCountOfBLocks - $currentRowBlocks;
+        }
+    }
+
+    return $watter;
+}
+
+/**
+ * This function is prepared to work with any size of array.
+ * We will return null if array has zero elements
+ * @param array $blocksCountByRow
+ * @return int|null
+ */
+function findTheHighestBlockNumberBeingIncremental(array $blocksCountByRow): ?int {
+    if (count($blocksCountByRow) === 0) {
+        return null;
+    }
+
+    $previousNumber = null;
+
+    foreach ($blocksCountByRow as $rowBlockCount) {
+        if ($previousNumber !== null && $previousNumber > $rowBlockCount) {
+            return $previousNumber;
+        }
+        $previousNumber =  $rowBlockCount;
+    }
+    return $previousNumber;
+}
