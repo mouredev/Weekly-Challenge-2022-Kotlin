74b39b6a4dcb995637bae93c54bb3973a4c8b0f0
Brais Moure <mouredev@gmail.com>
2022-11-14 23:06:38 +0100

    Resolución Reto #45 y enunciado Reto #46

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge45.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge45.kt
@@ -31,3 +31,42 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main() {
+    println(calculateWaterUnits(arrayOf(4, 0, 3, 6)))
+    println(calculateWaterUnits(arrayOf(4, 0, 3, 6, 1, 3)))
+    println(calculateWaterUnits(arrayOf(5, 4, 3, 2, 1, 0)))
+    println(calculateWaterUnits(arrayOf(0, 1, 2, 3, 4, 5)))
+    println(calculateWaterUnits(arrayOf(4, 0, 3, 6, 1, 3, 0, 1, 6)))
+}
+
+private fun calculateWaterUnits(container: Array<Int>): Int {
+
+    var units = 0
+    var wall = 0
+    var nextWall = 0
+
+    container.forEachIndexed { index, blocks ->
+
+        if (blocks < 0) {
+            return@forEachIndexed
+        }
+
+        if (index != container.size - 1 && (index == 0 || nextWall == blocks)) {
+
+            wall = if (index == 0) blocks else nextWall
+
+            nextWall = 0
+            for (nextBlocksIndex in index + 1 until container.size) {
+                if (container[nextBlocksIndex] >= nextWall) {
+                    nextWall = container[nextBlocksIndex]
+                }
+            }
+        } else {
+            val referenceWall = if (nextWall > wall) wall else nextWall
+            val currentBlocks = referenceWall - blocks
+            units += if (currentBlocks >= 0) currentBlocks else 0
+        }
+    }
+
+    return units
+}
