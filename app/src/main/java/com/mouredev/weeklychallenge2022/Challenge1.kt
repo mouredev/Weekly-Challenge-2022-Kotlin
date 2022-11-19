af192684dfd622f1a85be35f0f3a4954e536c8ba
Juanjo Torres <jjtr96@gmail.com>
2022-11-19 02:56:02 +0100

    Resolve challenge

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
@@ -20,13 +20,56 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
-fun main() {
-    println(isAnagram("amor", "roma"))
+static void Main(string[] args)
+{
+    Console.WriteLine($"Result: {IsAnagram("ael", "tale")}");
 }
 
-private fun isAnagram(wordOne: String, wordTwo: String): Boolean {
-    if (wordOne.lowercase() == wordTwo.lowercase()) {
-        return false
+private static bool IsAnagram(string wordOne, string wordTwo)
+{
+    wordOne = wordOne.ToLower();
+    wordTwo = wordTwo.ToLower();
+
+    if (IsSameWordOrDifferentLength(wordOne, wordTwo))
+    {
+        return false;
     }
-    return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
+
+    bool isAnagram = true;
+    List<char> wordOneLetters = new();
+    wordOneLetters.AddRange(wordOne);
+
+    foreach (char wordTwoletter in wordTwo)
+    {
+        if (LetterExistRemove(wordOneLetters, wordTwoletter))
+        {
+            isAnagram = false;
+            break;
+        }
+    }
+
+    return isAnagram;
 }
+
+#region Methods Complementaries
+private static bool LetterExistRemove(List<char> wordOneLetters, char wordTwoletter)
+{
+    return !wordOneLetters.Remove(wordTwoletter);
+}
+
+private static bool IsSameWordOrDifferentLength(string wordOne, string wordTwo)
+{
+    return IsSameWord(wordOne, wordTwo) ||
+                    !IsSameLength(wordOne, wordTwo);
+}
+
+private static bool IsSameLength(string wordOne, string wordTwo)
+{
+    return wordOne.Length == wordTwo.Length;
+}
+
+private static bool IsSameWord(string wordOne, string wordTwo)
+{
+    return wordOne == wordTwo;
+}
+#endregion
