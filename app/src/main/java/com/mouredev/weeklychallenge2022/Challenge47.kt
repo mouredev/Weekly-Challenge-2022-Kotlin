eae93a0f8b8b66f9e3b8bae1dc7fbf2fa29f3741
hameitsme <hernanamujicae@gmail.com>
2022-11-23 19:01:07 +0100

    reto-47-VOCAL-MAS-COMUN

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
@@ -16,4 +16,59 @@ package com.mouredev.weeklychallenge2022
  * - Tienes toda la información sobre los retos semanales en
  *   https://retosdeprogramacion.com/semanales2022.
  *
- */ 
+ */ def encuentra_vocal (frase):
+
+    frase = frase1.lower()
+
+    vocales = 'aeiouáéíóú'
+
+    vocal_add = [0,0,0,0,0,0,0,0,0,0]
+
+    for letrafrase in frase :
+
+     
+        for letravocales in vocales:
+
+            if letrafrase == letravocales :
+
+                indexvocal_add = vocales.index(letravocales)
+
+                vocal_add[indexvocal_add] +=1
+
+
+    index1eravocal = vocal_add.index(max(vocal_add))
+
+    if vocal_add[index1eravocal] == 0 :
+
+        print('No hay vocales en esta frase')
+
+    else :
+
+
+        print('La vocal que más se repite es la:',vocales[index1eravocal])
+
+
+
+    vocal_add[index1eravocal] *= 0 #ELIMINO 1ERA VOCAL MAS REPETIDA PARA ENCONTRAR LA SEGUNDA 
+
+
+
+    index2da_vocal = vocal_add.index(max(vocal_add))
+
+    if vocal_add[index2da_vocal] == 0:
+
+        print('No hay más vocales en esta frase')
+
+
+    else:
+
+
+        print('La segunda vocal que más se repite es la:',vocales[index2da_vocal])
+
+
+
+        #LLAMO A LA FUNCION 'encuentra_vocal
+
+
+frase1 = 'Encontrar y contabilizar las vocales presentes en esta frase, incluyendo vocales acentuadas : á é í ó ú'
+encuentra_vocal(frase1)
