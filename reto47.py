eccd1643f1f6f49ace88e91bb228a56cafdc5ac2
jiork16 <jordan_r_a@hotmail.es>
2022-11-24 16:08:25 -0500

    Optimizacion de codigo, que permite normalizar el texto

--- reto47.py
+++ reto47.py
@@ -2,14 +2,15 @@
 #Crear una lista de las vocales
 #Recorrer esa lista y preguntar cuantas veces se repite dicha vocal en la lista
 #hacer uso de la funcion max o crear una funcion que recorrar el numero maximo
-string = "sdfcyafewee"
+from unicodedata import normalize
+string = "sdfcyafeweèaaaaàààààààààà"
 def search_vocal_more_repit(texto: str):
-  list_vocal  = ["a","e","i","o","u","A","E","I","O","U","á","é","í","ó","ú","Á","É","Í","Ó","Ú"]
+  list_vocal  = ["a","e","i","o","u"]
   my_dict = dict()
   val_return=""
   for vocal in list_vocal:
     if texto.count(vocal) >0:
-      my_dict[vocal]=texto.count(vocal)
+      my_dict[vocal]=normalize("NFD", texto).count(vocal)
   
   if len(my_dict)==0:
     val_return=""
