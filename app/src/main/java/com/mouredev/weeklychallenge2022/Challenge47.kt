040583f979e4869187db341373f4d27f40c7bd5b
Felix Escandon <96452288+Fenixlix@users.noreply.github.com>
2022-11-26 14:47:41 -0500

    Solution Challenge#47

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
@@ -17,3 +17,62 @@ package com.mouredev.weeklychallenge2022
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+fun main(){
+    mostRepeatedVocalIn("Enunciado: Crea un función que reciba un texto y retorne la vocal que más veces se repita." +
+            " * Si no hay vocales podrá devolver vacío.")
+    mostRepeatedVocalIn("")
+}
+
+private fun mostRepeatedVocalIn(textForAnalysis : String) : Char?{
+    val totalLettersCharMap = mutableMapOf<Char, Int>()
+
+    fun addOrCount(letter : Char){
+        if (totalLettersCharMap.containsKey(letter)){
+            totalLettersCharMap[letter] = totalLettersCharMap[letter]!!.plus(1)
+        } else {
+            totalLettersCharMap[letter] = 1
+        }
+    }
+
+    textForAnalysis.split(" ").forEach {
+        it.toCharArray().forEach { singleLetter ->
+            addOrCount(singleLetter)
+        }
+    }
+
+    var mostRepeated : Char? = null
+    var totalCount = 0
+
+    fun computeMostRepeatedVocal(){
+        val vocals = listOf('a', 'e', 'i', 'o', 'u')
+        vocals.forEach{ vocal ->
+            if (totalLettersCharMap.containsKey(vocal)){
+                if (totalLettersCharMap[vocal]!! > totalCount){
+                    mostRepeated = vocal
+                    totalCount = totalLettersCharMap[vocal]!!
+
+                    println("$vocal , $totalCount")
+                }
+            }
+        }
+    }
+
+    fun computeMostRepeatedLetter(){
+        totalLettersCharMap.forEach{
+            if (it.value > totalCount){
+                totalCount = it.value
+                mostRepeated = it.key
+            }
+        }
+    }
+
+    computeMostRepeatedVocal()
+    if (mostRepeated != null)
+        println("Int the text : \"$textForAnalysis\", " +
+            "the most repeated vocal is $mostRepeated $totalCount times")
+    else
+        println("Null")
+
+    return mostRepeated
+}
