9cd44505974d51276e8689429d97c2ecaf8774d9
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2022-11-28 08:59:09 -0400

    Solución Reto #47 Dart
    Solución Reto #47 Dart

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge47.kt
@@ -1,19 +1,79 @@
 package com.mouredev.weeklychallenge2022
 
-/*
- * Reto #47
- * VOCAL MÁS COMÚN
- * Fecha publicación enunciado: 21/11/22
- * Fecha publicación resolución: 28/11/22
- * Dificultad: FÁCIL
- *
- * Enunciado: Crea un función que reciba un texto y retorne la vocal que más veces se repita.
- * Si no hay vocales podrá devolver vacío.
- *
- * Información adicional:
- * - Usa el canal de nuestro Discord (https://mouredev.com/discord) "🔁reto-semanal"
- *   para preguntas, dudas o prestar ayuda a la comunidad.
- * - Tienes toda la información sobre los retos semanales en
- *   https://retosdeprogramacion.com/semanales2022.
- *
- */
+//Crea un función que reciba un texto y retorne 
+//la vocal que más veces se repita.
+
+void main() {
+  vocalMasRepetida('marioi');
+  vocalMasRepetida('marioo');
+  vocalMasRepetida('eskerni');
+  vocalMasRepetida('jaziel');
+}
+
+void vocalMasRepetida(String cadena) {
+  var vocales = <int>[0, 0, 0, 0, 0];
+  String masRepetida = "";
+
+  for (int contador = 0; contador < cadena.length; contador++) {
+    switch (cadena[contador]) {
+      case 'a':
+        vocales[0]++;
+        break;
+      case 'A':
+        vocales[0]++;
+        break;
+      case 'e':
+        vocales[1]++;
+        break;
+      case 'E':
+        vocales[1]++;
+        break;
+      case 'i':
+        vocales[2]++;
+        break;
+        case 'I':
+        vocales[2]++;
+        break;
+      case 'o':
+        vocales[3]++;
+        break;
+        case 'O':
+        vocales[3]++;
+        break;
+      case 'u':
+        vocales[4]++;
+        break;
+        case 'U':
+        vocales[4]++;
+        break;
+    }
+  }
+
+  int mayor = 0;
+  String vocal = "";
+
+  for (int contador = 0; contador < vocales.length; contador++) {
+    if (vocales[contador] > mayor) {
+      mayor = vocales[contador];
+      switch (contador) {
+        case 0:
+          vocal = "a";
+          break;
+        case 1:
+          vocal = "e";
+          break;
+        case 2:
+          vocal = "i";
+          break;
+        case 3:
+          vocal = "o";
+          break;
+        case 4:
+          vocal = "u";
+          break;
+      }
+    }
+  }
+
+  print('la vocal mas repetida es $vocal');
+}
