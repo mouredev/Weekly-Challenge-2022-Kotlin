f9988169c000b49ad80bd58998d5d38dfa04da7c
Brais Moure <mouredev@gmail.com>
2022-11-28 21:42:20 +0100

    Resolución Reto #47 y enunciado Reto #48

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge48.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge48.kt
@@ -1,15 +1,24 @@
 package com.mouredev.weeklychallenge2022
 
 /*
- * Reto #47
- * VOCAL MÁS COMÚN
- * Fecha publicación enunciado: 21/11/22
- * Fecha publicación resolución: 28/11/22
+ * Reto #48
+ * EL CALENDARIO DE ADEVIENTO 2022
+ * Fecha publicación enunciado: 28/11/22
+ * Fecha publicación resolución: 05/12/22
  * Dificultad: FÁCIL
  *
- * Enunciado: Crea un función que reciba un texto y retorne la vocal que más veces se repita.
- * - Ten cuidado con algunos casos especiales.
- * - Si no hay vocales podrá devolver vacío.
+ * ¿Conoces el calendario de adviento de la comunidad (https://adviento.dev)?
+ * 24 días, 24 regalos sorpresa relacionados con desarrollo de software, ciencia y tecnología desde el 1 de diciembre.
+ *
+ * Enunciado: Crea una función que reciba un objeto de tipo "Date" y retorne lo siguiente:
+ * - Si la fecha coincide con el calendario de aDEViento 2022: Retornará el regalo de ese día (a tu elección) y cuánto queda para que finalice el sorteo de ese día.
+ * - Si la fecha es anterior: Cuánto queda para que comience el calendario.
+ * - Si la fecha es posterior: Cuánto tiempo ha pasado desde que ha finalizado.
+ *
+ * Notas:
+ * - Tenemos en cuenta que cada día del calendario comienza a medianoche 00:00:00 y finaliza a las 23:59:59.
+ * - Debemos trabajar con fechas que tengan año, mes, día, horas, minutos y segundos.
+ * - 🎁 Cada persona que aporte su solución entrará en un nuevo sorteo del calendario de aDEViento hasta el día de su corrección (sorteo exclusivo para quien entregue su solución).
  *
  * Información adicional:
  * - Usa el canal de nuestro Discord (https://mouredev.com/discord) "🔁reto-semanal"
