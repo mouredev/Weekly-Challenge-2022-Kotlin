211aedc2307dc3640199e416919ec5ef342d1f0a
pardoblanco <albertopb1943@gmail.com>
2022-12-15 11:46:32 +0100

    Añado la funcion decrypt en el ejercicio 50 de python

--- app/src/main/java/com/mouredev/weeklychallenge2022/ejercicio50.py
+++ app/src/main/java/com/mouredev/weeklychallenge2022/ejercicio50.py
@@ -32,7 +32,19 @@ def encrypt(text):
     final_text = " ".join(final_text)
     print(final_text)
 
+def decrypt(text):
+
+    lista_text = text.split()
+    list_less_aca = [elem[:-3] for elem in lista_text]
+    list_less_aca = " ".join(list_less_aca)
+
+    r_text = list_less_aca.maketrans("01223", "aeiou")
+    replaced_text = list_less_aca.translate(r_text)
+
+    print(replaced_text[::-1])
+
 
 if __name__ == "__main__":
     text = input("Introduzca un texto para encriptar")
     encrypt(text)
+    decrypt(text)
