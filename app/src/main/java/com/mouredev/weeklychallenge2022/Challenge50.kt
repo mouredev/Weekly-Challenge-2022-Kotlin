a5d62550a5450783b23efc22e1bf4e791ca0369c
Brais Moure <mouredev@gmail.com>
2022-12-19 22:14:54 +0100

    Resolución Reto #50 y enunciado Reto #51

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge50.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge50.kt
@@ -3,7 +3,7 @@ package com.mouredev.weeklychallenge2022
 /*
  * Reto #50
  * LA ENCRIPTACIÓN DE KARACA
- * Fecha publicación enunciado: 12/11/22
+ * Fecha publicación enunciado: 12/12/22
  * Fecha publicación resolución: 19/12/22
  * Dificultad: FÁCIL
  *
@@ -17,3 +17,46 @@ package com.mouredev.weeklychallenge2022
  *   https://retosdeprogramacion.com/semanales2022.
  *
  */
+
+fun main() {
+
+    println(karaca("placa", false))
+    println(karaca("0c0lpaca", true))
+
+    println(karaca("Este es el penúltimo reto de programación del año", false))
+    println(karaca("1ts1aca s1aca l1aca 3m2tlún1paca 3t1raca 1daca nó2c0m0rg3rpaca l1daca 3ñ0aca", true))
+
+    // El algoritmo no soporta estos casos
+    println(karaca("1", false))
+    println(karaca("1aca ", true))
+}
+
+private fun karaca(text: String, isKaraca: Boolean): String {
+
+    var result = ""
+
+    text.lowercase().split(" ").forEach { word ->
+
+        if (isKaraca) {
+            result += word
+                .dropLast(3)
+                .replace("0", "a")
+                .replace("1", "e")
+                .replace("2", "i")
+                .replace("3", "o")
+                .replace("4", "u")
+                .reversed().plus(" ")
+        } else {
+            result += word
+                .reversed()
+                .replace("a", "0")
+                .replace("e", "1")
+                .replace("i", "2")
+                .replace("o", "3")
+                .replace("u", "4")
+                .plus("aca ")
+        }
+    }
+
+    return result
+}
