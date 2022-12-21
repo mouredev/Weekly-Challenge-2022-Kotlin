eaa8e9ca8ae484e88b52277e2d18e8b9445fdf52
jcdm <jcdiezdemedina@gmail.com>
2022-12-21 15:41:52 -0400

    Reto-51-retos-para-el-2023

---
+++ reto51.py
@@ -0,0 +1,37 @@
+#
+# Reto #51
+# CONJETURA DE COLLATZ  3x + 1
+# Fecha publicación enunciado: --/--/--
+# Fecha publicación resolución: --/--/--
+# Dificultad: FACIL
+# Enunciado: La conjetura establece que no importa con qué número comience, siempre alcanzará 1 
+# crea un función que imprima el número de pasos y sus valores para llegar a 1
+# El procedimiento es: Dado un numero entero positivo digamos "n". 
+#   Si n es par, divida n por 2 para obtener n / 2. 
+#   Si n es impar, multiplique n por 3 y agregue 1 para obtener 3n + 1. 
+#
+# Ejemplo: n = 5
+# 1. 3 * 5 + 1 = 16
+# 2. 16 / 2 = 8
+# 3. 8 / 2 = 4
+# 4. 4 / 2 = 2
+# 5. 2 / 2 = 1
+# 
+# Deberá imprimir:
+# 1 16
+# 2 8
+# 3 4
+# 4 2
+# 5 1
+#
+#---------------------------------------------------------------------------------------
+#
+# Reto #51
+# NUMEROS ROMANOS
+# Fecha publicación enunciado: --/--/--
+# Fecha publicación resolución: --/--/-
+# Dificultad: FACIL
+# Enunciado: Crea una función que sea capaz de convertir un número entero positivo entre
+# 1 y 5000 a numeros romanos
+#
+
