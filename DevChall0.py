98fe7a720ae4a1d4800f792a1577755c8c738cb1
IronRyze <118767424+IronRyze@users.noreply.github.com>
2022-12-22 11:00:02 +0100

    Fix fizzbuzz error

--- DevChall0.py
+++ DevChall0.py
@@ -6,12 +6,16 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
 '''
-for i in range(1, 101):
-    if i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
-    elif i % 5 == 0 and i % 3 == 0:
-        print('fizzbuzz')
-    else:
-        print(i)
+def fizz_buzz():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        
+        else:
+            print(i)
+
+fizz_buzz()
