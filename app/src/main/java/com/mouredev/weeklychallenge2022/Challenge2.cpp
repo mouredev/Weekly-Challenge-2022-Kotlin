c4e1e7320ce71e25bfe11f51b5fd257474c4a03a
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2023-01-05 01:27:56 -0500

    Solución Reto #2 C++

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "??reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+long fibonacci(long num);
+
+int main() {
+    for (int i = 0; i < 50; i++)
+        cout << i << ": " << fibonacci(i) << endl;
+}
+
+long fibonacci(long num) {
+    if (num == 0) {
+        return 0;
+    }
+    else if (num == 1) {
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
