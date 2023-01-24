654cf979624fe9bd9f590d698623f396e210dde3
david <lightspot@gmail.com>
2023-01-24 19:11:15 +0100

    Solución Reto #[6] [Java]

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge6.java
@@ -0,0 +1,32 @@
+/*
+ * Reto #6
+ * INVIRTIENDO CADENAS
+ * Fecha publicación enunciado: 07/02/22
+ * Fecha publicación resolución: 14/02/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+public class Challenge6 {
+    public static void main(String[] args) {
+
+        System.out.println(wordInv("Hola Mundo!"));
+        
+    }
+    static String wordInv (String word){
+        String result = "";
+        for (int i = word.length()-1; i >= 0; i--) {
+            result += Character.toString(word.charAt(i));
+        }
+        return result;
+    }
+}
