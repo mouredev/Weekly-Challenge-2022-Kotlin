304c7a69d78e8d1c096a24c39e64faa9fb84e308
R3mus999 <126987358+R3mus999@users.noreply.github.com>
2023-03-04 22:37:19 +0100

    Challenge[0][Python]

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -19,19 +19,13 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
-fun main() {
 
-    for (index in 1..100) {
-        val divisibleByThree = index % 3 == 0
-        val divisibleByFive = index % 5 == 0
-        if (divisibleByThree && divisibleByFive) {
-            println("fizzbuzz")
-        } else if (divisibleByThree) {
-            println("fizz")
-        } else if (divisibleByFive) {
-            println("buzz")
-        } else {
-            println(index)
-        }
-    }
-}
+for n in range(1,101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("fizzbuzz")
+    elif n % 3 == 0 :
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)
