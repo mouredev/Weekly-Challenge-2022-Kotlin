b1ac7d1e750978579b69b03fbb0399cc96e3f74e
RicVal <none@none.com>
2023-03-24 16:13:41 -0600

    Simplificación de archivos

---
+++ python-soluciones/challenge_01.py
@@ -0,0 +1,20 @@
+"""
+    Reto #1
+    ¿ES UN ANAGRAMA?
+    Fecha publicación enunciado: 2022/01/
+    Fecha publicación resolución: 2023/03/09
+    Dificultad: MEDIA
+
+    Enunciado: Escribe una función que reciba dos palabras (String)
+        y retorne verdadero o falso (Boolean) según sean o no anagramas.
+    Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+    NO hace falta comprobar que ambas palabras existan.
+    Dos palabras exactamente iguales no son anagrama.
+"""
+
+def main():
+    """Función principal"""
+
+
+if __name__ == "__main__":
+    main()
