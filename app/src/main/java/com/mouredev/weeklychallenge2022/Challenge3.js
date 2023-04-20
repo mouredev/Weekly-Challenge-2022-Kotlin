af4ee508fe516214c1d279e094cb2639766af9fe
Yorman Londono <yorsh355@gmail.com>
2023-04-20 15:29:15 -0500

    Desafio #5 js

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge3.js
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge3.js
@@ -1,28 +1,11 @@
-// Función para comprobar si un número es primo
-function esPrimo(numero) {
-  if (numero <= 1) {
-    return false;
+const esPrimo = (n) =>{
+  if(n <= 1) return false;
+  for (let i = 2; i <= n / 2 ; i++){
+    if(n % i === 0) return false;
   }
-  for (let i = 2; i <= numero / 2; i++) {
-    if (numero % i === 0) {
-      return false;
-    }
-  }
-  return true;
+ return true;
 }
 
-// Comprobar si un número es primo
-const numero = 23; // Cambiar el número aquí para comprobar
-if (esPrimo(numero)) {
-  console.log(numero + " es un número primo.");
-} else {
-  console.log(numero + " no es un número primo.");
-}
-
-// Imprimir los números primos entre 1 y 100
-console.log("Números primos entre 1 y 100:");
-for (let i = 1; i <= 100; i++) {
-  if (esPrimo(i)) {
-    console.log(i);
-  }
+for (let i = 1 ; i <= 100; i++){
+  if(esPrimo(i)) console.log(i);
 }
