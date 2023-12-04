6e923a1a3a97444d771e8fe51ce1053bbb27faa4
alvarr <alvaro.aguilar@goumh.umh.es>
2023-12-05 00:44:22 +0100

    Reto 7 2022

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge7.py
@@ -0,0 +1,40 @@
+"""/*
+ * Reto #7
+ * CONTANDO PALABRAS
+ * Fecha publicación enunciado: 14/02/22
+ * Fecha publicación resolución: 21/02/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que cuente cuantas veces se repite cada palabra y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que lo resuelvan automáticamente.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */"""
+
+
+frase = "Esto es es, una! esto prueba"
+frase = frase.lower()
+
+for caracter in frase:
+    if caracter < 'a' or caracter > 'z':
+        frase = frase.replace(caracter, " ")
+
+tokens = frase.split()
+
+contador = {}
+
+for palabra in tokens:
+    if palabra in contador:
+        contador[palabra] += 1
+    else:
+        contador[palabra] = 0
+
+for entrada in contador:
+    print(entrada,":" , contador[entrada])
